7d1cd56dd21dd54ce4b4ce1cbc2e3033ed7641ab
summermong <2514o_o@naver.com>
2024-07-02 00:25:21 +0900

    chore: husky 적용

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,23 @@
-module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+// module.exports = {
+//   // Type check TypeScript files
+//   '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx)': (filenames) => {
-    return [
-      `npx eslint --fix ${filenames.join(' ')}`,
-      `npx prettier --write ${filenames.join(' ')}`,
-    ]
-  },
+//   // Lint & Prettify TS and JS files
+//   '**/*.(ts|tsx)': (filenames) => {
+//     return [
+//       `npx eslint --fix ${filenames.join(' ')}`,
+//       `npx prettier --write ${filenames.join(' ')}`,
+//     ]
+//   },
+// }
+
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{ts,tsx}': [buildEslintCommand],
 }
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'design',
+      ],
+    ],
+  },
+}
