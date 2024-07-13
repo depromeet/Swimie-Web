e76da524f89c7082d9e1ae33a68c71607f4842ae
hjy0951 <hjy0951@gmail.com>
2024-07-14 02:15:18 +0900

    chore: 프리텐다드 localFont 추가

---
+++ app/styles/font.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: '../../assets/fonts/Pretendard/PretendardVariable.woff2',
+  display: 'swap',
+  variable: '--pretendard',
+});
