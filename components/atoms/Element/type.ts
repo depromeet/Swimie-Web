af62916e102e6bcace354ea6f53548419bf3e5ce
brian <90752841+wokbjso@users.noreply.github.com>
2024-07-14 17:25:40 +0900

    fix: addStyles type 수정

--- components/atoms/Element/type.ts
+++ components/atoms/Element/type.ts
@@ -4,5 +4,5 @@ export interface SelectElementProps
   extends Pick<SelectListProps, 'value' | 'onSelect'> {
   isSelected: boolean;
   label: string;
-  addStyles?: string;
+  addStyles?: object;
 }
