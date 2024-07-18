e1f3ceca97108b5b6e29282b29e7d1f3dfb25c06
summermong <2514o_o@naver.com>
2024-07-18 22:54:03 +0900

    chore: tab 컴포넌트

---
+++ components/molecules/Tab/TabItem.tsx
@@ -0,0 +1,69 @@
+import { css } from '@/styled-system/css';
+
+import { ClickTabItemProps } from './type';
+
+export const TabItem = ({
+  selected,
+  text,
+  onClick,
+  type,
+}: ClickTabItemProps) => {
+  const tabItemStyles = css({
+    width: '100%',
+    height: type === 'primary' ? '56px' : '38px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    padding: type === 'primary' ? '16px 10px' : '8px 10px',
+    backgroundColor:
+      type === 'primary' && selected
+        ? 'white'
+        : type === 'secondary' && selected
+          ? 'white'
+          : type === 'assistive' && selected
+            ? 'coolNeutral.25'
+            : '',
+    borderBottom:
+      type === 'primary' && selected
+        ? '1px solid'
+        : type === 'secondary' && selected
+          ? ''
+          : '',
+    border: type === 'assistive' ? '1px solid' : '',
+    borderColor:
+      type === 'primary' && selected
+        ? 'blue.60'
+        : type === 'secondary' && selected
+          ? ''
+          : 'line.normal',
+    borderRadius:
+      type === 'primary' && selected
+        ? ''
+        : type === 'secondary' && selected
+          ? '10px'
+          : type === 'assistive'
+            ? '999px'
+            : '',
+    shadow:
+      type === 'primary' && selected
+        ? ''
+        : type === 'secondary' && selected
+          ? 'normal'
+          : '',
+    color:
+      type === 'primary' && selected
+        ? 'text.normal'
+        : type === 'secondary' && selected
+          ? 'text.normal'
+          : type === 'assistive' && selected
+            ? 'background.white'
+            : 'text.normal',
+  });
+
+  return (
+    <div className={tabItemStyles} onClick={onClick}>
+      {text}
+    </div>
+  );
+};
