0577af5147de0baa46d0f0547d7bd73cb4bebf51
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-07-21 17:49:41 +0900

    feat: bottomSheet component 구현

---
+++ components/molecules/bottom-sheet/sheet-dim.tsx
@@ -0,0 +1,22 @@
+import { MAX_LOOT_LAYOUT } from '@/constants';
+import { css } from '@/styled-system/css';
+
+const dimStyle = css({
+  position: 'fixed',
+  inset: '0',
+  zIndex: '800',
+  display: 'block',
+  width: '100%',
+  maxWidth: `${MAX_LOOT_LAYOUT}px`,
+  height: '100%',
+  margin: '0 auto',
+  backgroundColor: 'rgb(0 0 0 / 40%)',
+  animation: 'dimFadeIn 0.3s',
+});
+
+type Dim = {
+  onClick: () => void;
+};
+export const Dim = ({ onClick }: Dim) => {
+  return <div className={dimStyle} onClick={onClick} />;
+};
