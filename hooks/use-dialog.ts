f93834748094afbe9067dec416f06da1257a873d
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-07-23 01:26:37 +0900

    feat: dialog component 개발 및 사용을 위한 hook, store 추가

---
+++ hooks/use-dialog.ts
@@ -0,0 +1,46 @@
+'use client';
+
+import { useAtom } from 'jotai';
+
+import { DialogProps } from '@/components/molecules';
+import { dialogAtom } from '@/store';
+
+/**
+ * @description Dialog를 조작하기 위해 사용합니다.
+ *
+ * 사용 방법은 아래와 같습니다.
+ * 1. components
+ * const { dialogState } = useDialog();
+ *
+ * return <Dialog {...dialogState}>
+ *
+ * 2. hooks
+ * const { dialog } = useDialog();
+ *
+ * const clickOpenDialog = () => {
+ *  dialog({
+ *    ...options
+ *  })
+ * }
+ */
+type DialogOptions = Omit<DialogProps, 'isOpen' | 'onClose'>;
+export const useDialog = () => {
+  const [dialogState, setDialogState] = useAtom(dialogAtom);
+
+  const dialog = (options: DialogOptions) => {
+    setDialogState({
+      ...options,
+      isOpen: true,
+      onClose: close,
+    });
+  };
+
+  const close = () => {
+    setDialogState((prev) => ({
+      ...prev,
+      isOpen: false,
+    }));
+  };
+
+  return { dialogState, dialog, close } as const;
+};
