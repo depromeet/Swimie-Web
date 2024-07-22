f90212b9e2bb74d07d21a19093efdda36839050b
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-07-23 01:26:37 +0900

    feat: dialog component 개발 및 사용을 위한 hook, store 추가

---
+++ components/molecules/dialog/dialog.tsx
@@ -0,0 +1,105 @@
+import { Button, Dim } from '@/components/atoms';
+import { css } from '@/styled-system/css';
+import { flex, grid } from '@/styled-system/patterns';
+
+type Button = {
+  text: string;
+  onClick: () => void;
+};
+export type DialogProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  description?: string;
+  buttons?: {
+    confirm?: Button;
+    cancel?: Button;
+  };
+  isDim?: boolean;
+};
+
+export const Dialog = ({
+  isOpen,
+  onClose,
+  title,
+  description,
+  buttons = {},
+  isDim = true,
+}: DialogProps) => {
+  if (!isOpen) return null;
+  return (
+    <>
+      <div className={containerStyle}>
+        <div className={contentStyle}>
+          <div className={content.textStyle}>
+            <h1 className={text.titleStyle}>{title}</h1>
+            {description && (
+              <p className={text.descriptionStyle}>{description}</p>
+            )}
+          </div>
+
+          {Object.keys(buttons).length && (
+            <div className={content.buttonStyle}>
+              {buttons.confirm && (
+                <Button onClick={buttons.confirm.onClick}>
+                  {buttons.confirm.text}
+                </Button>
+              )}
+              {buttons.cancel && (
+                <Button onClick={buttons.cancel.onClick}>
+                  {buttons.cancel.text}
+                </Button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      {isOpen && isDim && <Dim onClick={onClose} />}
+    </>
+  );
+};
+
+const containerStyle = css({
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  translate: 'auto',
+  translateX: '-1/2',
+  translateY: '-1/2',
+  padding: '24px',
+  zIndex: 1000,
+  backgroundColor: 'white',
+  borderRadius: '12px',
+  // TEMP: 임시 효과
+  animation: 'dimFadeIn 0.3s',
+});
+
+const contentStyle = flex({
+  direction: 'column',
+  gap: '24px',
+});
+
+const content = {
+  textStyle: css({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '4px',
+  }),
+
+  buttonStyle: grid({
+    gap: '12px',
+    gridTemplateColumns: 2,
+  }),
+};
+
+const text = {
+  titleStyle: css({
+    textStyle: 'heading4',
+    fontWeight: 'bold',
+  }),
+
+  descriptionStyle: css({
+    textStyle: 'body2.normal',
+    fontWeight: 'medium',
+  }),
+};
