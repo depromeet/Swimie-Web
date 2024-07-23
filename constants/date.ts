c18f803f0d1262cc61b0ddeb0a7c984f06388251
hjy0951 <hjy0951@gmail.com>
2024-07-23 20:50:43 +0900

    design: 달력 스켈레톤 컴포넌트 구현

---
+++ constants/date.ts
@@ -0,0 +1,4 @@
+export const START_OF_DAY = 0;
+export const END_OF_DAY = 6;
+
+export const nameOfDays = ['일', '월', '화', '수', '목', '금', '토'];
---
+++ features/main/calendar/atoms/calendar-item.tsx
@@ -0,0 +1,42 @@
+import { css } from '@/styled-system/css';
+
+interface CalendarItemProps {
+  date: number;
+  content?: string;
+  distance?: number;
+}
+
+export const CalendarItem = ({
+  date,
+  content,
+  distance,
+}: CalendarItemProps) => {
+  return (
+    <div className={itemContainer}>
+      <p className={css({ marginBottom: '5px' })}>{date}</p>
+      {/* TODO: 이후 상세 기록 페이지로 이동하는 Link 변경 필요 */}
+      <div className={linkContainer}>{content && <p>{content}</p>}</div>
+      {distance && <p>{distance}</p>}
+    </div>
+  );
+};
+
+const itemContainer = css({
+  width: 'full',
+  display: 'flex',
+  flexDir: 'column',
+  alignItems: 'center',
+
+  '& > p': {
+    height: '16px',
+    textStyle: 'caption2',
+    fontWeight: 'medium',
+  },
+});
+
+const linkContainer = css({
+  width: 'full',
+  height: '63px',
+  backgroundColor: 'background.gray',
+  rounded: '8px',
+});
