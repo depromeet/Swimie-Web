3d382d6bb4c73fc572588563c042a48b78edf76e
hjy0951 <hjy0951@gmail.com>
2024-07-24 22:12:14 +0900

    refactor: 현재 월에 대한 달력을 그리기 위한 로직을 훅으로 분리

--- features/main/calendar/molecules/calendar.tsx
+++ features/main/calendar/molecules/calendar.tsx
@@ -1,34 +1,13 @@
 'use client';
 
-import dayjs from 'dayjs';
-import { useAtomValue } from 'jotai';
-
-import { calendarDateAtom } from '@/store';
+import { useCalendarRendaringData } from '@/hooks/use-calendar-rendering-data';
 import { css } from '@/styled-system/css';
 
 import { CalendarItem, DayLabels } from '../atoms';
 import { CalendarHeader } from './calendar-header';
 
-const CALENDAR_FORMAT_5_COLUMNS = 35;
-const CALENDAR_FORMAT_6_COLUMNS = 42;
-
 export const Calendar = () => {
-  const currentDate = useAtomValue(calendarDateAtom);
-
-  const { year, month } = currentDate;
-  const dateObj = dayjs(`${year}-${month}-01`);
-  const startPoint = dateObj.get('day');
-  const totalDays = dateObj.add(1, 'month').subtract(1, 'day').get('date');
-  const endPoint = startPoint + totalDays - 1;
-  const squares = Array.from(
-    {
-      length:
-        endPoint >= CALENDAR_FORMAT_5_COLUMNS
-          ? CALENDAR_FORMAT_6_COLUMNS
-          : CALENDAR_FORMAT_5_COLUMNS,
-    },
-    (_, i) => i + 1,
-  );
+  const [squares, startPoint, endPoint] = useCalendarRendaringData();
 
   return (
     <div className={calendarContainer}>
