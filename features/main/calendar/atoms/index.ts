b02d1fb1cfa5a66a7781d286978e50f5304b825f
hjy0951 <hjy0951@gmail.com>
2024-07-24 20:51:14 +0900

    design: 체크 아이콘 컴포넌트 및 달력 사진보기 버튼 컴포넌트 추가

--- features/main/calendar/atoms/index.ts
+++ features/main/calendar/atoms/index.ts
@@ -1,3 +1,4 @@
 export { CalendarItem } from './calendar-item';
 export { DayLabels } from './day-labels';
 export { MoveMonthButton } from './move-month-button';
+export { ViewImageButton } from './view-image-button';
---
+++ features/main/calendar/atoms/view-image-button.tsx
@@ -0,0 +1,34 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import { CheckInCircleIcon } from '@/components/atoms/icons/check-in-circle-icon';
+import { css } from '@/styled-system/css';
+
+interface ViewImagehButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isActive: boolean;
+}
+
+export const ViewImageButton = ({
+  isActive,
+  ...props
+}: ViewImagehButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={css({
+        display: 'flex',
+        gap: '4px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textStyle: 'label2',
+        fontWeight: 'medium',
+        color: isActive ? 'primary.swim.총거리.default' : 'text.alternative',
+
+        '& > p': { height: '18px' },
+      })}
+    >
+      <CheckInCircleIcon isActive={isActive} />
+      <p>사진 보기</p>
+    </button>
+  );
+};
