18585feecad0b544a28c6c6bb296f7a14214666a
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-07-25 00:46:15 +0900

    refactor: 불필요 코드 삭제

--- app/record-detail/[id]/page.tsx
+++ app/record-detail/[id]/page.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { BottomSheet } from '@/components/molecules';
 import {
   DetailDescriptionSection,
   DetailDiarySection,
   DetailPreviewSection,
 } from '@/features/record-detail';
-import { useBottomSheet } from '@/hooks';
 import { flex } from '@/styled-system/patterns';
 
 const RecordDetail = () => {
-  const [isOpen, open, close] = useBottomSheet();
-
   return (
     <article className={containerStyle}>
       {/* preview section */}
@@ -20,19 +16,6 @@ const RecordDetail = () => {
       <DetailDescriptionSection />
       {/* diary section */}
       <DetailDiarySection />
-
-      <button onClick={open}>bottom sheet toggle button</button>
-      <BottomSheet
-        isOpen={isOpen}
-        direction="top"
-        onClose={close}
-        header={{
-          title: 'title test',
-          description: 'description',
-        }}
-      >
-        <div style={{ height: 300 }}>hello</div>
-      </BottomSheet>
     </article>
   );
 };
