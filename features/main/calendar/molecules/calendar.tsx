77a7fff3e3b2a2cb26645cabfded33239d617662
hjy0951 <hjy0951@gmail.com>
2024-07-24 15:48:46 +0900

    feat: day.js를 이용하여 실제 달력과 같이 표기하도록 로직 추가

--- features/main/calendar/molecules/calendar.tsx
+++ features/main/calendar/molecules/calendar.tsx
@@ -1,22 +1,58 @@
+'use client';
+
+import dayjs from 'dayjs';
+import { useState } from 'react';
+
 import { css } from '@/styled-system/css';
 
 import { CalendarHeader, CalendarItem, DayLabels } from '../atoms';
 
+const CALENDAR_FORMAT_5_COLUMNS = 35;
+const CALENDAR_FORMAT_6_COLUMNS = 42;
+
+interface CalendarDate {
+  year: number;
+  month: number;
+}
+
 export const Calendar = () => {
-  const arr = Array.from({ length: 30 }, (_, i) => i + 1);
+  const today = dayjs();
+  const [currentDate] = useState<CalendarDate>(() => {
+    return { year: today.get('year'), month: today.get('month') + 1 };
+  });
+
+  const { year, month } = currentDate;
+  const dateObj = dayjs(`${year}-${month}-01`);
+  const startPoint = dateObj.get('day');
+  const totalDays = dateObj.add(1, 'month').subtract(1, 'day').get('date');
+  const endPoint = startPoint + totalDays - 1;
+  const squares = Array.from(
+    {
+      length:
+        endPoint >= CALENDAR_FORMAT_5_COLUMNS
+          ? CALENDAR_FORMAT_6_COLUMNS
+          : CALENDAR_FORMAT_5_COLUMNS,
+    },
+    (_, i) => i + 1,
+  );
+
   return (
     <div className={calendarContainer}>
-      <CalendarHeader />
+      <CalendarHeader {...currentDate} />
       <DayLabels />
-      <div className={CalendarGrid}>
-        {arr.map((date) => (
-          <CalendarItem
-            key={date}
-            date={date}
-            distance={date % 2 === 0 ? 1000 : undefined}
-          />
-        ))}
-      </div>
+      <ul className={CalendarGrid}>
+        {squares.map((date, index) =>
+          index >= startPoint && index <= endPoint ? (
+            <CalendarItem
+              key={date}
+              date={date - startPoint}
+              distance={date % 2 === 0 ? 1000 : undefined}
+            />
+          ) : (
+            <div key={`out-of-range-${index}`} />
+          ),
+        )}
+      </ul>
     </div>
   );
 };
