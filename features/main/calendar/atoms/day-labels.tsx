477d506f3301b1f77330f2c57795786078f48c6d
hjy0951 <hjy0951@gmail.com>
2024-07-25 14:41:49 +0900

    refactor: 맵 자료구조 활용으로 삼항 연산자 중첩 제거

--- features/main/calendar/atoms/day-labels.tsx
+++ features/main/calendar/atoms/day-labels.tsx
@@ -1,6 +1,13 @@
 import { END_OF_DAY, nameOfDays, START_OF_DAY } from '@/constants/date';
 import { css, cva, cx } from '@/styled-system/css';
 
+type Weekend = 'sunday' | 'saturday' | undefined;
+
+const dayTypeMap = new Map<number, string>([
+  [START_OF_DAY, 'sunday'],
+  [END_OF_DAY, 'saturday'],
+]);
+
 export const DayLabels = () => {
   return (
     <div
@@ -12,29 +19,27 @@ export const DayLabels = () => {
         alignItems: 'center',
       })}
     >
-      {nameOfDays.map((day, index) => (
-        <p
-          key={index}
-          className={cx(
-            css({
-              width: 'full',
-              textAlign: 'center',
-              textStyle: 'label2',
-              fontWeight: 'medium',
-            }),
-            colorOfDays({
-              type:
-                index === START_OF_DAY
-                  ? 'sunday'
-                  : index === END_OF_DAY
-                    ? 'saturday'
-                    : undefined,
-            }),
-          )}
-        >
-          {day}
-        </p>
-      ))}
+      {nameOfDays.map((day, index) => {
+        const type = (dayTypeMap.get(index) || undefined) as Weekend;
+        return (
+          <p
+            key={index}
+            className={cx(
+              css({
+                width: 'full',
+                textAlign: 'center',
+                textStyle: 'label2',
+                fontWeight: 'medium',
+              }),
+              colorOfDays({
+                type,
+              }),
+            )}
+          >
+            {day}
+          </p>
+        );
+      })}
     </div>
   );
 };
