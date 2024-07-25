19c8bbfdb92ba1ac980e35c0a70831594fac3f25
brian <90752841+wokbjso@users.noreply.github.com>
2024-07-25 21:35:23 +0900

    chore: subInfo Textfield open 상태 관리하는 커스텀 훅에 description 추가

--- features/record/hooks/use-sub-info-text-fields.tsx
+++ features/record/hooks/use-sub-info-text-fields.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from 'react';
 
+/**
+ * @description subInfo(심박수, 페이스, 칼로리) 필드 open 상태 관리 custom-hook
+ */
 export function UseSubInfoTextFields() {
   const [isOpen, setIsOpen] = useState(false);
 
