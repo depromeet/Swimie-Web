ab1413873fac0e768753c037ea1d9cd21ae1c3cd
summermong <2514o_o@naver.com>
2024-07-25 22:48:27 +0900

    chore: button 컴포넌트 초기화

--- components/atoms/button/button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonHTMLAttributes, forwardRef } from 'react';
-
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ ...props }, ref) => <button ref={ref} {...props} />,
-);
-
-Button.displayName = 'Button';
