69bc79f61f502b97116623f0b4f63971e0dd0ab3
summermong <2514o_o@naver.com>
2024-07-28 01:04:42 +0900

    chore: button 컴포넌트 ui 테스트를 위한 storybook 적용

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -12,6 +13,15 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '..'),
+      };
+    }
+    return config;
   },
 };
 
