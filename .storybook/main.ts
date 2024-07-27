65ae80b76589b350b2d227ab558b3df1927a94d8
summermong <2514o_o@naver.com>
2024-07-28 00:53:54 +0900

    chore: 불필요한 main.ts 설정 코드 삭제

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -13,15 +12,6 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/nextjs',
     options: {},
-  },
-  webpackFinal: async (config) => {
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, '..'),
-      };
-    }
-    return config;
   },
 };
 
