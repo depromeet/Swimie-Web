9fe8ac580d583a660c6afa80f4c80dd83dbf3b43
summermong <2514o_o@naver.com>
2024-07-28 00:47:20 +0900

    refactor: button 컴포넌트 코드 리팩토링

---
+++ components/atoms/button/type.ts
@@ -0,0 +1,11 @@
+export interface ButtonProps {
+  size: 'large' | 'medium' | 'small';
+  disabled?: boolean;
+  label: string;
+  interaction: 'normal' | 'hovered' | 'focused' | 'pressed';
+  variant?: 'solid' | 'outlined' | 'text';
+  type?: 'primary' | 'secondary' | 'assistive';
+  leftIconSrc?: string;
+  rightIconSrc?: string;
+  onClick?: () => void;
+}
