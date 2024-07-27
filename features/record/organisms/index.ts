b56f877a4f611cffa385b5e08bd271ec6319800b
brian <90752841+wokbjso@users.noreply.github.com>
2024-07-28 01:36:32 +0900

    chore: index.ts 파일 수정

--- features/record/organisms/index.ts
+++ features/record/organisms/index.ts
@@ -5,5 +5,7 @@ export * from './equipment-section';
 export * from './form';
 export * from './lane-length-bottom-sheet';
 export * from './photo-section';
+export * from './pool-search-page-modal';
+export * from './pool-search-result-list';
 export * from './sub-info-section';
 export * from './sub-info-text-fields';
