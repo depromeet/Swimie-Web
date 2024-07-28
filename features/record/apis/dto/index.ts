758c46ba24780c712294941b6974c106f7535711
Kim Hyeonmin <90752841+wokbjso@users.noreply.github.com>
2024-07-29 00:07:23 +0900

    feat: search-bar 컴포넌트를 생성
    * feat: search-bar 컴포넌트 생성
    
    * chore: search-bar 컴포넌트 주석 추가
    
    * chore: flex pattern 사용
    
    * chore: index.ts 파일 수정
    
    * feat: 수영장 검색 page-modal 구현
    
    * feat: 수영 검색 page-modal 내용 생성
    
    * chore: 수영 검색 페이지 모달 외부 스타일 주입 요소 삭제
    
    * refactor: poolSearchText에 따라 렌더링 분기처리
    
    * feat: react-error-boundary 설치
    
    * feat: 수영 검색 결과 form에 반영 로직 구현
    
    * chore: 이후 step 주석 추가
    
    * chore: index.ts 파일 수정

--- features/record/apis/dto/index.ts
+++ features/record/apis/dto/index.ts
@@ -1,1 +1,2 @@
+export * from './pool-search';
 export * from './record';
--- features/record/components/organisms/index.ts
+++ features/record/components/organisms/index.ts
@@ -3,6 +3,7 @@ export * from './equipment-section';
 export * from './form';
 export { default as LaneLengthBottomSheet } from './lane-length-bottom-sheet';
 export * from './photo-section';
-export * from './pool-search-page-modal';
+export { default as PoolSearchPageModal } from './pool-search-page-modal';
+export * from './pool-search-result-list';
 export * from './sub-info-section';
 export * from './sub-info-text-fields';
