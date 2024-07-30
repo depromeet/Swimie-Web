1332959e7ce12c826fc854bb2f9bb94c6c45e636
hjy0951 <hjy0951@gmail.com>
2024-07-30 17:38:55 +0900

    chore: github dummy image 활용을 위한 config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['github.com'], // TODO: github dummy image test를 위한 config로 기능 구현 후 제거 필요
+  },
+};
 
 export default nextConfig;
