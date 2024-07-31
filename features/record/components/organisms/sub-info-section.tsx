19b4e6c1cc068534d1bce5235df5aee65eda63c6
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-01 03:52:02 +0900

    feat: 장비 기록 기능 구현

--- features/record/components/organisms/sub-info-section.tsx
+++ features/record/components/organisms/sub-info-section.tsx
@@ -1,95 +1,40 @@
 'use client';
 
-import { AnimatePresence, motion } from 'framer-motion';
-import { useFormContext } from 'react-hook-form';
-
 import { DownArrowIcon } from '@/components/atoms';
 import { Divider } from '@/components/atoms/divider';
-import { TextField } from '@/components/molecules';
 import { css } from '@/styled-system/css';
-import { flex } from '@/styled-system/patterns';
 
 import { useSubInfoTextFields } from '../../hooks';
 import { FormSectionProps } from '../../types/form-section';
+import { SubInfoTextFields } from './sub-info-text-fields';
 
 export function SubInfoSection({ title }: FormSectionProps) {
   const { isOpen, handlers } = useSubInfoTextFields();
-  const { getValues } = useFormContext();
-
-  const handleTextFieldsOpenStateClick = () => {
-    handlers.onChangeFieldsOpen();
-  };
 
   return (
-    <>
+    <section>
       <div
-        className={titleStyles.layout}
-        onClick={handleTextFieldsOpenStateClick}
+        className={beforeExpandStyles.layout}
+        onClick={() => handlers.onChangeFieldsOpen()}
       >
-        <h1 className={titleStyles.text}>{title}</h1>
+        <h1 className={beforeExpandStyles.title}>{title}</h1>
         <DownArrowIcon />
       </div>
-      <AnimatePresence>
-        {!isOpen && (
-          <motion.div
-            layout
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] }}
-          >
-            <Divider variant="thick" />
-          </motion.div>
-        )}
-        {isOpen && (
-          <motion.div
-            layout
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] }}
-            className={textFieldsStyles}
-          >
-            <TextField
-              label="심박수"
-              unit="BPM"
-              value={
-                getValues('heartRate') ? String(getValues('heartRate')) : ''
-              }
-              wrapperClassName={css({ marginBottom: '23px' })}
-              onChange={handlers.onChangeHeartRate}
-            />
-            <TextField
-              label="페이스"
-              unit="/100m"
-              value={getValues('pace') ? (getValues('pace') as string) : ''}
-              wrapperClassName={css({ marginBottom: '23px' })}
-              onChange={handlers.onChangePace}
-            />
-            <TextField
-              label="칼로리"
-              unit="Kcal"
-              value={getValues('kcal') ? String(getValues('kcal')) : ''}
-              onChange={handlers.onChangeKcal}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </>
+      {!isOpen && <Divider variant="thick" />}
+      <SubInfoTextFields isOpen={isOpen} />
+    </section>
   );
 }
 
-const titleStyles = {
-  layout: flex({
+const beforeExpandStyles = {
+  layout: css({
+    display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '20px 24px',
+    padding: '24px 20px',
   }),
-
-  text: css({
+  title: css({
     textStyle: 'heading4',
     fontWeight: '600',
   }),
 };
-
-const textFieldsStyles = css({
-  padding: '0 20px 24px 20px',
-});
