e049a7bcef66116782460f2d4eb3f437a9edc7b5
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-01 03:56:01 +0900

    chore: 주석 추가

--- features/record/components/molecules/equipment-select-box.tsx
+++ features/record/components/molecules/equipment-select-box.tsx
@@ -2,7 +2,7 @@ import { SwimIcon } from '@/components/atoms';
 import { css, cva, cx } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
 
-interface SelectBoxProps {
+interface EquipmentSelectBoxProps {
   label: string;
   index: number;
   isSelected: boolean;
@@ -10,13 +10,20 @@ interface SelectBoxProps {
   onSelectEquipment: (index: number) => void;
 }
 
+/**
+ * @param label 장비 box의 label값
+ * @param index 장비의 index
+ * @param isSelected 장비가 선택되었는지 여부
+ * @param className 외부 스타일 주입
+ * @param onSelectEquipment 클릭 된 장비의 index를 넘겨받는 function
+ */
 export function EquipmentSelectBox({
   label,
   index,
   isSelected,
   className,
   onSelectEquipment,
-}: SelectBoxProps) {
+}: EquipmentSelectBoxProps) {
   const handleEquipmentClick = () => {
     onSelectEquipment?.(index);
   };
