0982f405f9846f6df304771c42e8b86aed647e1a
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-01 04:08:14 +0900

    feat: accordion atom  컴포넌트로 분리

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,3 +1,4 @@
+export * from './accordion';
 export * from './button';
 export * from './dim';
 export * from './icons';
--- features/record/components/organisms/sub-info-section.tsx
+++ features/record/components/organisms/sub-info-section.tsx
@@ -1,40 +1,84 @@
 'use client';
 
-import { DownArrowIcon } from '@/components/atoms';
+import { AnimatePresence } from 'framer-motion';
+import { useFormContext } from 'react-hook-form';
+
+import { Accordion, DownArrowIcon } from '@/components/atoms';
 import { Divider } from '@/components/atoms/divider';
+import { TextField } from '@/components/molecules';
 import { css } from '@/styled-system/css';
+import { flex } from '@/styled-system/patterns';
 
 import { useSubInfoTextFields } from '../../hooks';
 import { FormSectionProps } from '../../types/form-section';
-import { SubInfoTextFields } from './sub-info-text-fields';
 
 export function SubInfoSection({ title }: FormSectionProps) {
   const { isOpen, handlers } = useSubInfoTextFields();
+  const { getValues } = useFormContext();
+
+  const handleTextFieldsOpenStateClick = () => {
+    handlers.onChangeFieldsOpen();
+  };
 
   return (
-    <section>
+    <>
       <div
-        className={beforeExpandStyles.layout}
-        onClick={() => handlers.onChangeFieldsOpen()}
+        className={titleStyles.layout}
+        onClick={handleTextFieldsOpenStateClick}
       >
-        <h1 className={beforeExpandStyles.title}>{title}</h1>
+        <h1 className={titleStyles.text}>{title}</h1>
         <DownArrowIcon />
       </div>
-      {!isOpen && <Divider variant="thick" />}
-      <SubInfoTextFields isOpen={isOpen} />
-    </section>
+      <AnimatePresence>
+        {!isOpen && (
+          <Accordion>
+            <Divider variant="thick" />
+          </Accordion>
+        )}
+        {isOpen && (
+          <Accordion className={textFieldsStyles}>
+            <TextField
+              label="심박수"
+              unit="BPM"
+              value={
+                getValues('heartRate') ? String(getValues('heartRate')) : ''
+              }
+              wrapperClassName={css({ marginBottom: '23px' })}
+              onChange={handlers.onChangeHeartRate}
+            />
+            <TextField
+              label="페이스"
+              unit="/100m"
+              value={getValues('pace') ? (getValues('pace') as string) : ''}
+              wrapperClassName={css({ marginBottom: '23px' })}
+              onChange={handlers.onChangePace}
+            />
+            <TextField
+              label="칼로리"
+              unit="Kcal"
+              value={getValues('kcal') ? String(getValues('kcal')) : ''}
+              onChange={handlers.onChangeKcal}
+            />
+          </Accordion>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
 
-const beforeExpandStyles = {
-  layout: css({
-    display: 'flex',
+const titleStyles = {
+  layout: flex({
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '24px 20px',
+    padding: '20px 24px',
   }),
-  title: css({
+
+  text: css({
     textStyle: 'heading4',
     fontWeight: '600',
   }),
 };
+
+const textFieldsStyles = css({
+  padding: '0 20px 24px 20px',
+});
