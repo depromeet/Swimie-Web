fa4e9a754cd41a1b36480aa4fb7a6b70b9faf17c
summermong <2514o_o@naver.com>
2024-08-01 17:42:18 +0900

    feat: refreshToken 없을 시 /login으로 라우팅되도록 미들웨어 설정

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const refreshToken = request.cookies.get('refreshToken')?.value;
+
+  if (!refreshToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/'],
+};
