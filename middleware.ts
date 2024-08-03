f3b17a91226a5f244333e0c8f8d7f1b57bea6cf1
summermong <2514o_o@naver.com>
2024-08-03 21:16:17 +0900

    fix: 주석 수정 및 헤더 설정

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,8 @@ import { NextResponse } from 'next/server';
 import { NewTokenData } from './apis/refresh-token';
 
 export async function middleware(request: NextRequest) {
-  const cookies = request.cookies;
-  let accessToken = cookies.get('accessToken')?.value;
-  const refreshToken = cookies.get('refreshToken')?.value;
+  let accessToken = request.cookies.get('accessToken')?.value;
+  const refreshToken = request.cookies.get('refreshToken')?.value;
 
   const loginPageRegex = /^\/login$/;
   const isLoginPage = loginPageRegex.test(request.nextUrl.pathname);
@@ -16,7 +15,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // TODO: accessToken 재발급
+  // NOTE: accessToken 재발급
   if (refreshToken && !accessToken) {
     const responseData = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/login/refresh`,
@@ -30,21 +29,22 @@ export async function middleware(request: NextRequest) {
     );
 
     const data = (await responseData.json()) as NewTokenData;
-    accessToken = data.data.accessToken;
+    accessToken = `Bearer ${data.data.accessToken}`;
 
     const response = NextResponse.next();
-    response.cookies.set('accessToken', `Bearer ${accessToken}`, {
+    response.cookies.set('accessToken', accessToken, {
       maxAge: 3600,
       httpOnly: true,
       secure: true,
     });
 
-    // TODO: refresh 사용하여 access 토큰 발급 + 헤더에 세팅
+    response.headers.set('Authorization', accessToken);
+
+    // NOTE: 로그인 페이지일 경우, '/'경로로 리다이렉트
     if (isLoginPage) {
-      // TODO: 로그인 페이지일 경우, '/'경로로 리다이렉트
       return NextResponse.redirect(new URL('/', request.url));
     }
-    // TODO: 로그인 페이지가 아닌 경우, 토큰 발급만 진행
+    // NOTE: 로그인 페이지가 아닌 경우, 토큰 발급만 진행
     return response;
   }
 }
