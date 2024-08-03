136d47410942a52dd4909b5847a8d9037291af08
summermong <2514o_o@naver.com>
2024-08-03 18:16:17 +0900

    fix: page <-> index 파일명 수정

--- app/kakao/oauth/index.tsx
+++ app/kakao/oauth/index.tsx
@@ -1,7 +1,43 @@
-import dynamic from 'next/dynamic';
+'use client';
 
-const DynamicKakaoOAuthPage = dynamic(() => import('./page'), { ssr: false });
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default function KakaoOAuthPage() {
-  return <DynamicKakaoOAuthPage />;
-}
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const KAKAO_CODE = searchParams.get('code');
+
+    const postCode = async () => {
+      if (!KAKAO_CODE) {
+        console.error('코드가 유효하지 않습니다.');
+        return;
+      }
+      try {
+        const response = await fetch(`/api/kakao/oauth?code=${KAKAO_CODE}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ code: KAKAO_CODE }),
+        });
+
+        if (response.status === 200) {
+          router.push('/');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    postCode().catch((error) => {
+      console.error('Error:', error);
+    });
+  }, [router, searchParams]);
+
+  return <div>카카오톡으로 로그인중입니다.</div>;
+};
+
+export default Page;
--- app/kakao/oauth/page.tsx
+++ app/kakao/oauth/page.tsx
@@ -1,43 +1,7 @@
-'use client';
+import dynamic from 'next/dynamic';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+const DynamicKakaoOAuthPage = dynamic(() => import('.'), { ssr: false });
 
-const Page = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const KAKAO_CODE = searchParams.get('code');
-
-    const postCode = async () => {
-      if (!KAKAO_CODE) {
-        console.error('코드가 유효하지 않습니다.');
-        return;
-      }
-      try {
-        const response = await fetch(`/api/kakao/oauth?code=${KAKAO_CODE}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ code: KAKAO_CODE }),
-        });
-
-        if (response.status === 200) {
-          router.push('/');
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-    postCode().catch((error) => {
-      console.error('Error:', error);
-    });
-  }, [router, searchParams]);
-
-  return <div>카카오톡으로 로그인중입니다.</div>;
-};
-
-export default Page;
+export default function KakaoOAuthPage() {
+  return <DynamicKakaoOAuthPage />;
+}
