c7f3dec11d6dfcc452fc4a86323a886cf59d86e8
hjy0951 <hjy0951@gmail.com>
2024-08-05 15:01:08 +0900

    feat: 타임라인 API 연결

---
+++ features/main/time-line/types/index.ts
@@ -0,0 +1,37 @@
+import { MemberInfo, MemoryType } from '../../calendar';
+
+export interface StrokeInfo {
+  strokeId: number;
+  name: string;
+  laps: number;
+  meter: number;
+}
+
+export interface TimeLineResponse {
+  status: number;
+  code: string;
+  message: string;
+  data: {
+    member: MemberInfo;
+    content: Array<TimeLineContent>;
+    pageSize: number;
+    cursorRecordAt: number;
+    hasNext: boolean;
+  };
+}
+
+export interface TimeLineContent {
+  memoryId: number;
+  type: MemoryType;
+  recordAt: string;
+  startTime?: string;
+  endTime?: string;
+  lane: number;
+  diary?: string;
+  totalDistance?: number;
+  memoryDetailId: number;
+  kcal?: number;
+  strokes?: Array<StrokeInfo>;
+  isAchieved?: boolean;
+  images: Array<string>;
+}
