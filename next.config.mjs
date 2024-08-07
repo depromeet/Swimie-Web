dfeb8decbc019bdf034478030a407257210e5615
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-07 13:18:55 +0900

    chore: remotePatterns 적용

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   images: {
     domains: ['github.com'], // TODO: github dummy image test를 위한 config로 기능 구현 후 제거 필요
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.cloudfront.net' },
+      { protocol: 'http', hostname: '**.cloudfront.net' },
+    ],
   },
 };
 
