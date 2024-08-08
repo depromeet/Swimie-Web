dcb40cb78f25a65609d12614deb4c47189278604
summermong <2514o_o@naver.com>
2024-08-08 12:43:46 +0900

    feat: 소셜 로그인 성공 시 응답의 닉네임 추가

--- store/auth.ts
+++ store/auth.ts
@@ -2,10 +2,12 @@ import { atom } from 'jotai';
 
 export interface AuthInfo {
   isLogined: boolean;
+  userName: string;
   userId: number;
 }
 
 export const AuthInfoAtom = atom<AuthInfo>({
   isLogined: false,
+  userName: '',
   userId: 0,
 });
