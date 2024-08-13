33eb4bfd88665276ba9fc4177a69c9f15a3c2680
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-14 01:39:52 +0900

    feat: follow, following 탭 추가

---
+++ features/follow/sections/follow-tab.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Tab, TabItem } from '@/components/molecules';
+
+import { type FollowTab } from '../types';
+
+type FollowTabSection = {
+  tab: FollowTab;
+};
+export const FollowTabSection = ({ tab }: FollowTabSection) => {
+  const router = useRouter();
+
+  const handleClickTab = (tab: FollowTab) => {
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set('tab', tab);
+
+    router.push(currentUrl.toString());
+  };
+
+  return (
+    <Tab>
+      <TabItem
+        text="팔로워"
+        selected={tab === 'follow'}
+        onClick={() => handleClickTab('follow')}
+      />
+      <TabItem
+        text="팔로잉"
+        selected={tab === 'following'}
+        onClick={() => handleClickTab('following')}
+      />
+    </Tab>
+  );
+};
---
+++ features/follow/types/index.ts
@@ -0,0 +1,1 @@
+export type FollowTab = 'follow' | 'following';
