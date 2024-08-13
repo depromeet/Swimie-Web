d46df56bed76cc26c6845da1564f54148a2f0f3e
hjy0951 <hjy0951@gmail.com>
2024-08-14 01:55:37 +0900

    feat: 타임라인 영법별 거리 통계 추가

--- features/main/time-line/atoms/swim-record-chart.tsx
+++ features/main/time-line/atoms/swim-record-chart.tsx
@@ -1,5 +1,6 @@
-import { css } from '@/styled-system/css';
-import { flex } from '@/styled-system/patterns';
+import { StrokeName } from '@/features/record-detail';
+import { css, cva } from '@/styled-system/css';
+import { flex, grid } from '@/styled-system/patterns';
 import { getSwimColor } from '@/utils';
 
 import { StrokeInfo } from '../types';
@@ -13,7 +14,7 @@ interface SwimRecordChartProps {
 
 interface SwimChartData {
   strokeId: number;
-  name: string;
+  name: StrokeName;
   ratio: number;
   color: string;
 }
@@ -39,19 +40,60 @@ export const SwimRecordChart = ({
     .sort((a, b) => b.ratio - a.ratio);
 
   return (
-    <div className={chartContainerStyles} style={{ width }}>
-      {chartData.map(({ name, ratio, color }) => (
-        <div
-          key={name}
-          style={{ width: `${ratio}%`, height: '20px', backgroundColor: color }}
-          className={css({ borderRadius: '2px' })}
-        />
-      ))}
-    </div>
+    <>
+      <div className={chartContainerStyles} style={{ width }}>
+        {chartData.map(({ name, ratio, color }) => (
+          <div
+            key={name}
+            style={{
+              width: `${ratio}%`,
+              height: '20px',
+              backgroundColor: color,
+            }}
+            className={css({ borderRadius: '2px' })}
+          />
+        ))}
+      </div>
+      {strokes.length > 1 && (
+        <ul className={statsContainerStyles}>
+          {strokes.map(({ strokeId, name, meter }) => (
+            <li key={strokeId} className={itemStyles}>
+              <p className={nameStyles({ type: name })}>{name}</p>
+              <p className={distanceStyles}>{meter}m</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
 const chartContainerStyles = flex({
   w: 'full',
   gap: '2px',
+});
+
+const statsContainerStyles = grid({
+  columns: 4,
+  rowGap: '10px',
+});
+
+const itemStyles = flex({ direction: 'column', gap: '2px' });
+
+const nameStyles = cva({
+  base: { textStyle: 'label1.normal', fontWeight: 'medium' },
+  variants: {
+    type: {
+      자유형: { color: 'primary.swim.자유형.default' },
+      배영: { color: 'primary.swim.배영.default' },
+      접영: { color: 'primary.swim.접영.default' },
+      평영: { color: 'primary.swim.평영.default' },
+      킥판: { color: 'primary.swim.킥판.default' },
+    },
+  },
+});
+
+const distanceStyles = css({
+  textStyle: 'label1.normal',
+  fontWeight: 'semibold',
 });
