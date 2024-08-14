0afd3c1b620e1177027ee408af0dd63d757ad8d0
summermong <2514o_o@naver.com>
2024-08-14 20:42:52 +0900

    feat: profile 마크업

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,96 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from '@/components/atoms';
+import BadgeIcon from '@/components/atoms/icons/badge-icon';
+import StatisticsIcon from '@/components/atoms/icons/statistics-icon';
+import { UserImageIcon } from '@/components/atoms/icons/user-image-icon';
+import { Tab, TabItem } from '@/components/molecules';
+import { css } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
 
 export default function Profile() {
+  const [selectedTab, setSelectedTab] = useState<'statistics' | 'badge'>(
+    'statistics',
+  );
+
+  const handleClickTab = (tab: 'statistics' | 'badge') => {
+    setSelectedTab(tab);
+  };
+
   return (
     <article className={containerStyle}>
-      <p>본인의 profile 입니다.</p>
+      <section className={profileStyles}>
+        <div className={infoStyles}>
+          <UserImageIcon />
+          <div>
+            <div className={nameStyles}>김스위미</div>
+            <div className={introStyles}>한 줄 소개</div>
+          </div>
+        </div>
+        <div className={friendInfoStyles}>
+          <div className={friendStyles}>
+            <div className={friendTypeStyles}>팔로워</div>
+            <div className={friendCountStyles}>0</div>
+          </div>
+          <div className={friendStyles}>
+            <div className={friendTypeStyles}>팔로잉</div>
+            <div className={friendCountStyles}>0</div>
+          </div>
+        </div>
+        <div className={buttonListStyles}>
+          <Button
+            size="small"
+            label="프로필 편집"
+            buttonType="assistive"
+            variant="outlined"
+            className={buttonStyle}
+          />
+          <Button
+            size="small"
+            label="프로필 공유"
+            buttonType="assistive"
+            variant="outlined"
+            className={buttonStyle}
+          />
+        </div>
+      </section>
+      <Tab type="primary" className={css({ padding: '20px' })}>
+        <TabItem
+          text="통계"
+          selected={selectedTab === 'statistics'}
+          type="primary"
+          variant="fill"
+          onClick={() => handleClickTab('statistics')}
+        />
+        <TabItem
+          text="배지"
+          selected={selectedTab === 'badge'}
+          type="primary"
+          variant="fill"
+          onClick={() => handleClickTab('badge')}
+        />
+      </Tab>
+      {selectedTab === 'statistics' && (
+        <div className={TabContentStyle}>
+          <StatisticsIcon />
+          <div className={descriptionStyles}>
+            <div className={titleStyles}>통계 기능이 곧 출시돼요!</div>
+            <div className={subtitleStyles}>그동안 꾸준히 기록해보세요</div>
+          </div>
+        </div>
+      )}
+
+      {selectedTab === 'badge' && (
+        <div className={TabContentStyle}>
+          <BadgeIcon />
+          <div className={descriptionStyles}>
+            <div className={titleStyles}>배지 기능이 곧 출시돼요!</div>
+            <div className={subtitleStyles}>그동안 꾸준히 기록해보세요</div>
+          </div>
+        </div>
+      )}
     </article>
   );
 }
@@ -11,4 +98,90 @@ export default function Profile() {
 const containerStyle = flex({
   direction: 'column',
   gap: '12px',
+});
+
+const profileStyles = flex({
+  direction: 'column',
+  alignItems: 'flex-start',
+  gap: '20px',
+  padding: '20px',
+});
+
+const infoStyles = flex({
+  alignItems: 'flex-start',
+  gap: '21px',
+  alignSelf: 'stretch',
+});
+
+const nameStyles = css({
+  textStyle: 'heading3',
+  fontWeight: '600',
+  color: 'text.normal',
+});
+
+const introStyles = css({
+  textStyle: 'body2.normal',
+  fontWeight: '500',
+  color: 'text.alternative',
+});
+
+const friendInfoStyles = flex({
+  alignItems: 'center',
+  gap: '40px',
+  alignSelf: 'stretch',
+});
+
+const friendStyles = flex({
+  direction: 'column',
+  alignItems: 'flex-start',
+  gap: '2px',
+});
+
+const friendTypeStyles = css({
+  textStyle: 'label2',
+  fontWeight: '500',
+  color: 'text.alternative',
+});
+
+const friendCountStyles = css({
+  textStyle: 'body2.normal',
+  fontWeight: '600',
+  color: 'text.normal',
+});
+
+const buttonListStyles = flex({
+  width: '100%',
+  alignItems: 'flex-start',
+  gap: '10px',
+  alignSelf: 'stretch',
+});
+
+const buttonStyle = css({
+  flexGrow: 1,
+});
+
+const TabContentStyle = flex({
+  direction: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '20px',
+  paddingTop: '80px',
+});
+
+const descriptionStyles = flex({
+  direction: 'column',
+  alignItems: 'center',
+  gap: '4px',
+  alignSelf: 'stretch',
+});
+
+const titleStyles = css({
+  textStyle: 'heading6',
+  color: 'text.normal',
+  fontWeight: '500',
+});
+
+const subtitleStyles = css({
+  textStyle: 'body2.normal',
+  color: 'text.alternative',
 });
