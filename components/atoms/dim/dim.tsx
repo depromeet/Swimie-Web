0c9b2ec719650113ea0cf333e2e17872d8f8ece5
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-16 23:09:06 +0900

    style: dim component의 쌓임 정책을 정의합니다

--- components/atoms/dim/dim.tsx
+++ components/atoms/dim/dim.tsx
@@ -1,21 +1,36 @@
 import { css } from '@/styled-system/css';
 
-const dimStyle = css({
-  position: 'fixed',
-  inset: '0',
-  zIndex: '800',
-  display: 'block',
-  width: '100%',
-  maxWidth: 'maxWidth',
-  height: '100%',
-  margin: '0 auto',
-  backgroundColor: 'rgb(0 0 0 / 40%)',
-  animation: 'dimFadeIn 0.3s',
-});
-
 type Dim = {
   onClick: () => void;
+  zIndex?: 500 | 600 | 700 | 800;
 };
-export const Dim = ({ onClick }: Dim) => {
-  return <div className={dimStyle} onClick={onClick} />;
+/**
+ *
+ * @description bottomSheet, modal, dialog에서 사용되는 Dim component입니다.
+ *
+ * @param zIndex Dim의 zIndex value입니다.
+ *   BottomSheet - dim: 500 / container: 550
+ *   Modal - dim: 700 / container: 750
+ *   Dialog - dim: 800 / container: 850
+ */
+export const Dim = ({ onClick, zIndex = 800 }: Dim) => {
+  return (
+    <div
+      onClick={onClick}
+      className={css({
+        position: 'fixed',
+        inset: '0',
+        display: 'block',
+        width: '100%',
+        maxWidth: 'maxWidth',
+        height: '100%',
+        margin: '0 auto',
+        backgroundColor: 'rgb(0 0 0 / 40%)',
+        animation: 'dimFadeIn 0.3s',
+      })}
+      style={{
+        zIndex,
+      }}
+    />
+  );
 };
