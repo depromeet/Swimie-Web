8544e701abf803351a51bbfcc7b8bad991460cd5
summermong <2514o_o@naver.com>
2024-08-19 23:31:49 +0900

    fix: 코드 수정

--- components/atoms/button/button.tsx
+++ components/atoms/button/button.tsx
@@ -167,15 +167,7 @@ export const Button = ({
     <button className={buttonStyles} onClick={onClick} type={type}>
       {(leftIconSrc || isLoading) && (
         <div className={iconWrapperStyles}>
-          {isLoading && <LoadingIcon />}
-          {leftIconSrc && (
-            <Image
-              src={leftIconSrc}
-              alt="left icon"
-              width={iconSize}
-              height={iconSize}
-            />
-          )}
+          {isLoading ? (
             <LoadingIcon />
           ) : (
             leftIconSrc && (
