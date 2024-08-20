5d5cf07d76a1322b3214677ebc4af594ba3edfcc
hjy0951 <hjy0951@gmail.com>
2024-08-20 21:08:02 +0900

    chore: variable -> static font로 변경

--- styles/font.ts
+++ styles/font.ts
@@ -1,7 +1,53 @@
 import localFont from 'next/font/local';
 
 export const pretendard = localFont({
-  src: '../assets/fonts/Pretendard/PretendardVariable.woff2',
+  src: [
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-Thin.woff',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-ExtraLight.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-Light.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-SemiBold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-ExtraBold.woff',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Pretendard/Pretendard-Black.woff',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
   display: 'swap',
   variable: '--pretendard',
 });
