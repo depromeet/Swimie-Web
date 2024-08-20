1b73dae9fce852ddcd58f0dc986f771ecaa1cad1
summermong <2514o_o@naver.com>
2024-08-20 16:01:32 +0900

    feat: 애플 로그인 관련 로직

---
+++ app/apple/oauth/index.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { useSetAtom } from 'jotai';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { LoadingArea } from '@/components/atoms';
+import { AuthInfoAtom } from '@/store/auth';
+import { flex } from '@/styled-system/patterns';
+import { AuthResponse } from '@/types/authType';
+
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const setAuth = useSetAtom(AuthInfoAtom);
+
+  useEffect(() => {
+    const APPLE_CODE = searchParams.get('code');
+    console.log(APPLE_CODE);
+
+    const postCode = async () => {
+      if (!APPLE_CODE) {
+        console.error('코드가 유효하지 않습니다.');
+        return;
+      }
+      try {
+        const response = await fetch(`/api/apple/oauth?code=${APPLE_CODE}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ code: APPLE_CODE }),
+        });
+
+        if (response.status === 200) {
+          const data = (await response.json()) as AuthResponse;
+
+          setAuth({
+            isLogined: true,
+            nickname: data.data.data.nickname,
+            userId: data.data.data.userId,
+          });
+          router.push('/join/nickname');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    postCode().catch((error) => {
+      console.error('Error:', error);
+    });
+  }, [router, searchParams, setAuth]);
+
+  return (
+    <div className={LoadingWrapper}>
+      <LoadingArea width={100} height={100} />
+      <div>Apple로 로그인중입니다.</div>
+    </div>
+  );
+};
+
+const LoadingWrapper = flex({
+  direction: 'column',
+  justifyContent: 'center',
+  alignContent: 'center',
+  height: '100vh',
+  textAlign: 'center',
+  fontWeight: 'bold',
+});
+
+export default Page;
