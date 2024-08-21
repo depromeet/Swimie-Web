ff7f498b67cb40988986170d143096d777007b8d
hjy0951 <hjy0951@gmail.com>
2024-08-21 22:53:29 +0900

    fix: 사라진 타입에 대한 조건 삭제

--- utils/icons.ts
+++ utils/icons.ts
@@ -2,7 +2,5 @@ import { Directions } from '@/components/atoms';
 
 export function getRotateDegree(direction: Directions) {
   if (direction === 'left') return 0;
-  else if (direction === 'up') return 90;
-  else if (direction === 'right') return 180;
-  return 270;
+  return 180;
 }
