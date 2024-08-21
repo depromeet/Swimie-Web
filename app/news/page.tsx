d30813121a58c0b67788b5da017fb6db2ae3a7bf
hjy0951 <hjy0951@gmail.com>
2024-08-21 22:30:59 +0900

    design: 소식 페이지 헤더 마크업

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,11 +1,24 @@
-import { NewsList } from '@/features/news';
+import { HeaderBar } from '@/components/molecules';
+import { FollowingListLinkButton, NewsList } from '@/features/news';
+import { FindMemberButton } from '@/features/news/components/atoms/find-member-button';
 import { css } from '@/styled-system/css';
 
 export default function NewsPage() {
   return (
-    <section className={sectionStyle}>
-      <NewsList />
-    </section>
+    <>
+      <HeaderBar>
+        <HeaderBar.LeftContent>
+          <FollowingListLinkButton />
+        </HeaderBar.LeftContent>
+        <HeaderBar.RightContent>
+          {[{ component: <FindMemberButton />, key: 'findMember' }]}
+        </HeaderBar.RightContent>
+      </HeaderBar>
+
+      <section className={sectionStyle}>
+        <NewsList />
+      </section>
+    </>
   );
 }
 
