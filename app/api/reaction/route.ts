ba39218def62f05674117984b1ebb486995c2ef0
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-21 23:23:08 +0900

    refactor: post route type 수정

--- app/api/reaction/route.ts
+++ app/api/reaction/route.ts
@@ -1,20 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { fetchData } from '@/apis/fetch-data';
-import { ImagePresignedResponse } from '@/features/record';
-import { DetailCheerItem } from '@/features/record-detail';
-
-type Cheer = {
-  memoryId: number;
-} & DetailCheerItem;
+import { Cheer } from '@/features/record-detail/apis';
 
 export async function POST(request: NextRequest) {
   const body = (await request.json()) as Promise<Cheer>;
-  const data = await fetchData<ImagePresignedResponse>(
-    `/reaction`,
-    'POST',
-    body,
-  );
+  const data = await fetchData<{
+    status: number;
+    code: 'REACTION_1' | 'REACTION_2';
+    message: string;
+  }>(`/reaction`, 'POST', body);
 
   return NextResponse.json(data);
 }
