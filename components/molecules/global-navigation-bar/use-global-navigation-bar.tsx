122db1d55a837a28840e17d1f3e9484c4f409ad9
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-23 14:52:13 +0900

    feat: 유저가 임의로 url 수정할 때를 대비하여 useEffect로직 추가

--- components/molecules/global-navigation-bar/use-global-navigation-bar.tsx
+++ components/molecules/global-navigation-bar/use-global-navigation-bar.tsx
@@ -1,19 +1,24 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useGlobalNavigationBar(itemRoutes: string[]) {
   const pathname = usePathname();
   const barIndexWithRouteMap = new Map([
     [itemRoutes[0], 0],
     [itemRoutes[1], 1],
-    [itemRoutes[2], 1],
+    [itemRoutes[2], 2],
   ]);
 
   const [barIndex, setBarIndex] = useState<number>(
     barIndexWithRouteMap.get(pathname) ?? 0,
   );
+
+  useEffect(() => {
+    setBarIndex(barIndexWithRouteMap.get(pathname) ?? 0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
 
   const onChangeBarIndex = (index: number) => {
     setBarIndex(index);
