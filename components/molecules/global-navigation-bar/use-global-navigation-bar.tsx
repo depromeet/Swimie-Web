d24247362cfed602ccdfcdc82561823fc5ce9de0
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-23 14:09:34 +0900

    chore: 불필요한 div 태그 제거

--- components/molecules/global-navigation-bar/use-global-navigation-bar.tsx
+++ components/molecules/global-navigation-bar/use-global-navigation-bar.tsx
@@ -1,12 +1,14 @@
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
+//Todo: 유저 id를 해당 훅의 매개변수로 받아온 후 Map에 라우팅 추가
 export function useGlobalNavigationBar() {
   const pathname = usePathname();
   const barIndexWithRouteMap = new Map([
     ['/', 0],
     ['/news', 1],
   ]);
+
   const [barIndex, setBarIndex] = useState<number>(
     barIndexWithRouteMap.get(pathname) ?? 0,
   );
