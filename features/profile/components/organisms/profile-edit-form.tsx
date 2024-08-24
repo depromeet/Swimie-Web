7f9af1f6ae33d9f84b3c9fef0d702e6a6125cd5e
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-24 17:03:25 +0900

    feat: 프로필 사진 선택 기능 구현

--- features/profile/components/organisms/profile-edit-form.tsx
+++ features/profile/components/organisms/profile-edit-form.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+import { ChangeEvent, useRef, useState } from 'react';
 import { SubmitHandler, useForm, useWatch } from 'react-hook-form';
 
-import { Button, DefaultImageIcon } from '@/components/atoms';
+import { Button, DefaultImageIcon, Image } from '@/components/atoms';
 import { UserImageIcon } from '@/components/atoms/icons/user-image-icon';
 import { FormTextArea, FormTextField } from '@/components/molecules';
 import { css } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
+import { resizeFile } from '@/utils';
 
 interface ProfileEditFormProps {
   nickname?: string;
@@ -21,21 +23,76 @@ export function ProfileEditForm() {
     defaultValues: {},
   });
 
+  const [image, setImage] = useState<string>();
+  const fileInput = useRef<HTMLInputElement>(null);
+
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const uploadImage = async () => {
+      try {
+        if (e.target.files) {
+          const targetFile = e.target.files[0];
+          if (targetFile) setImage('');
+          const resizedImage = await resizeFile(targetFile, 200, 200, 100);
+          const reader = new FileReader();
+          reader.onload = () => {
+            if (reader.readyState === FileReader.DONE) {
+              if (reader.result !== image) {
+                setImage(reader.result as string);
+              }
+            }
+          };
+          reader.readAsDataURL(resizedImage);
+        }
+      } catch (error) {
+        console.error('이미지 업로드 중 오류가 발생하였습니다', error);
+      }
+    };
+    uploadImage().catch((error) =>
+      console.error('이미지 업로드 중 오류가 발생하였습니다', error),
+    );
+  };
+
   // eslint-disable-next-line @typescript-eslint/require-await
   const onSubmit: SubmitHandler<ProfileEditFormProps> = async (data) => {
     console.log(data);
+  };
+
+  const handleAddImageClick = () => {
+    if (fileInput.current) {
+      fileInput.current.click();
+    }
   };
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <form onSubmit={handleSubmit(onSubmit)} className={layoutStyles.form}>
       <section className={layoutStyles.imageEdit}>
-        <div className={layoutStyles.imageEditIcon}>
-          <UserImageIcon width={100} height={100} />
+        <div
+          className={layoutStyles.imageEditIcon}
+          onClick={handleAddImageClick}
+        >
+          {image ? (
+            <Image
+              src={image}
+              alt="프로필 사진"
+              fill
+              sizes="40vw"
+              className={css({ borderRadius: 'full' })}
+            />
+          ) : (
+            <UserImageIcon width={100} height={100} />
+          )}
           <div className={layoutStyles.defaultImageIcon}>
             <DefaultImageIcon />
           </div>
         </div>
+        <input
+          ref={fileInput}
+          type="file"
+          accept="image/*"
+          className={css({ display: 'none' })}
+          onChange={handleImageUpload}
+        />
       </section>
       <FormTextField
         {...register('nickname')}
