d6a578d56a6b6819430c0ba306fa77c1ca412735
brian <90752841+wokbjso@users.noreply.github.com>
2024-08-25 21:47:49 +0900

    chore: 코드 리뷰 반영

--- features/record/apis/dto/memory.ts
+++ features/record/apis/dto/memory.ts
@@ -1,58 +1,50 @@
-export interface MemoryResponse {
-  status: number;
-  code: string;
-  message: string;
-  data: {
-    month: number;
-    rank: number;
-    memoryId: number;
-  };
-}
+import { Response } from '@/apis';
 
-export interface MemoryPullResponse {
-  status: number;
-  code: string;
-  message: string;
-  data: {
-    id: number;
-    member: {
-      goal: number;
-      name: string;
-    };
-    pool: {
-      id: number;
-      name: string;
-      address: string;
-      lane: number;
-    };
-    memoryDetail: {
-      item: string;
-      heartRate: number;
-      paceMinutes: number;
-      paceSeconds: number;
-      kcal: number;
-    };
-    strokes: [
-      {
-        strokeId: number;
-        name: string;
-        laps: number;
-        meter: number;
-      },
-    ];
-    images: [
-      {
-        imageName: string;
-        url: string;
-      },
-    ];
-    recordAt: string;
-    startTime: string;
-    endTime: string;
-    duration: string;
-    lane: number;
-    totalLap: number;
-    totalMeter: number;
-    diary: string;
+export type MemoryResponse = Response<{
+  month: number;
+  rank: number;
+  memoryId: number;
+}>;
+
+export type MemoryPullResponse = Response<{
+  id: number;
+  member: {
+    goal: number;
+    name: string;
   };
-}
+  pool: {
+    id: number;
+    name: string;
+    address: string;
+    lane: number;
+  };
+  memoryDetail: {
+    item: string;
+    heartRate: number;
+    paceMinutes: number;
+    paceSeconds: number;
+    kcal: number;
+  };
+  strokes: [
+    {
+      strokeId: number;
+      name: string;
+      laps: number;
+      meter: number;
+    },
+  ];
+  images: [
+    {
+      imageName: string;
+      url: string;
+    },
+  ];
+  recordAt: string;
+  startTime: string;
+  endTime: string;
+  duration: string;
+  lane: number;
+  totalLap: number;
+  totalMeter: number;
+  diary: string;
+}>;
