d6374047096318a9bbd301fad5f8502e6d5b8b9b
hjy0951 <hjy0951@gmail.com>
2024-08-27 00:45:41 +0900

    design: 빈 소식 페이지 추가

--- features/news/components/organisms/news-list.tsx
+++ features/news/components/organisms/news-list.tsx
@@ -3,205 +3,57 @@
 import { useRef } from 'react';
 
 import { PullToRefresh } from '@/components/atoms';
+import { HeaderBar } from '@/components/molecules';
 import { TimeLineCard, TimeLineContent } from '@/features/main';
+import { css } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
 
 import { NewsItem } from '../../types';
-import { NewsItemWrapper, NewsItemWrapperProps } from '../molecules';
-
-const dummys: Array<NewsItem> = [
-  {
-    memberId: 3,
-    memberNickName: 'heojoooon',
-    createdAt: '2024-08-21 00:00:00',
-    isRecentNews: true,
-    memoryId: 38,
-    recordAt: '2024-08-14',
-    startTime: '14:00',
-    endTime: '15:00',
-    lane: 25,
-    totalDistance: 750,
-    isAchieved: false,
-    type: 'MULTI',
-    diary: 'SWIMIE 화이팅~!~!~!~!!~!!~!~!~!~!~!~!',
-    strokes: [
-      {
-        strokeId: 108,
-        name: '자유형',
-        laps: 3,
-        meter: 150,
-      },
-      {
-        strokeId: 109,
-        name: '배영',
-        laps: 3,
-        meter: 150,
-      },
-      {
-        strokeId: 110,
-        name: '평영',
-        laps: 3,
-        meter: 150,
-      },
-      {
-        strokeId: 111,
-        name: '접영',
-        laps: 3,
-        meter: 150,
-      },
-      {
-        strokeId: 112,
-        name: '킥판',
-        laps: 3,
-        meter: 150,
-      },
-    ],
-    imageUrl: '',
-  },
-  {
-    memberId: 3,
-    memberNickName: 'heojoooon',
-    createdAt: '2024-08-20 18:00:00',
-    isRecentNews: true,
-    memoryId: 37,
-    recordAt: '2024-08-13',
-    startTime: '14:00',
-    endTime: '15:00',
-    lane: 25,
-    totalDistance: 800,
-    isAchieved: false,
-    type: 'MULTI',
-    diary: 'SWIMIE 화이팅~!~!~!~!!~!!~!~!~!~!~!~!',
-    strokes: [
-      {
-        strokeId: 108,
-        name: '자유형',
-        laps: 10,
-        meter: 500,
-      },
-      {
-        strokeId: 111,
-        name: '접영',
-        laps: 6,
-        meter: 300,
-      },
-    ],
-    imageUrl: '',
-  },
-  {
-    memberId: 3,
-    memberNickName: 'heojoooon',
-    createdAt: '2024-08-20 18:00:00',
-    isRecentNews: true,
-    memoryId: 371,
-    recordAt: '2024-08-13',
-    startTime: '14:00',
-    endTime: '15:00',
-    lane: 25,
-    totalDistance: 800,
-    isAchieved: false,
-    type: 'MULTI',
-    diary: 'SWIMIE 화이팅~!~!~!~!!~!!~!~!~!~!~!~!',
-    strokes: [
-      {
-        strokeId: 108,
-        name: '자유형',
-        laps: 10,
-        meter: 500,
-      },
-      {
-        strokeId: 111,
-        name: '접영',
-        laps: 6,
-        meter: 300,
-      },
-    ],
-    imageUrl: '',
-  },
-  {
-    memberId: 3,
-    memberNickName: 'heojoooon',
-    createdAt: '2024-08-20 18:00:00',
-    isRecentNews: true,
-    memoryId: 372,
-    recordAt: '2024-08-13',
-    startTime: '14:00',
-    endTime: '15:00',
-    lane: 25,
-    totalDistance: 800,
-    isAchieved: false,
-    type: 'MULTI',
-    diary: 'SWIMIE 화이팅~!~!~!~!!~!!~!~!~!~!~!~!',
-    strokes: [
-      {
-        strokeId: 108,
-        name: '자유형',
-        laps: 10,
-        meter: 500,
-      },
-      {
-        strokeId: 111,
-        name: '접영',
-        laps: 6,
-        meter: 300,
-      },
-    ],
-    imageUrl: '',
-  },
-  {
-    memberId: 3,
-    memberNickName: 'heojoooon',
-    createdAt: '2024-08-20 18:00:00',
-    isRecentNews: true,
-    memoryId: 373,
-    recordAt: '2024-08-13',
-    startTime: '14:00',
-    endTime: '15:00',
-    lane: 25,
-    totalDistance: 800,
-    isAchieved: false,
-    type: 'MULTI',
-    diary: 'SWIMIE 화이팅~!~!~!~!!~!!~!~!~!~!~!~!',
-    strokes: [
-      {
-        strokeId: 108,
-        name: '자유형',
-        laps: 10,
-        meter: 500,
-      },
-      {
-        strokeId: 111,
-        name: '접영',
-        laps: 6,
-        meter: 300,
-      },
-    ],
-    imageUrl: '',
-  },
-];
+import { EmptyNews, NewsItemWrapper, NewsItemWrapperProps } from '../molecules';
+import { FindMemberButton, FollowingListLinkButton } from '../atoms';
 
 export const NewsList = () => {
   const ptrRef = useRef(null);
-  const lastItemIndex = dummys.length - 1;
+  const data: Array<NewsItem> = [];
+  const isEmpty = data.length === 0;
+  const lastItemIndex = data.length - 1;
 
-  return (
-    <div ref={ptrRef}>
-      <PullToRefresh ref={ptrRef} onRefresh={() => console.log('refreshing')} />
-      <ol className={listStyles}>
-        {dummys.map((content, index) => {
-          const { wrapperProps, cardContent } = getPropsObjects(content);
-          return (
-            <NewsItemWrapper
-              key={cardContent.memoryId}
-              {...wrapperProps}
-              isLast={lastItemIndex === index}
-            >
-              <TimeLineCard content={cardContent} isViewDate={false} />
-            </NewsItemWrapper>
-          );
-        })}
-      </ol>
-    </div>
+  return isEmpty ? (
+    <section className={emptySectionStyle}>
+      <EmptyNews />
+    </section>
+  ) : (
+    <>
+      <HeaderBar>
+        <HeaderBar.LeftContent>
+          <FollowingListLinkButton />
+        </HeaderBar.LeftContent>
+        <HeaderBar.RightContent>
+          {[{ component: <FindMemberButton />, key: 'findMember' }]}
+        </HeaderBar.RightContent>
+      </HeaderBar>
+
+      <div className={sectionStyle} ref={ptrRef}>
+        <PullToRefresh
+          ref={ptrRef}
+          onRefresh={() => console.log('refreshing')}
+        />
+        <ol className={listStyles}>
+          {data.map((content, index) => {
+            const { wrapperProps, cardContent } = getPropsObjects(content);
+            return (
+              <NewsItemWrapper
+                key={cardContent.memoryId}
+                {...wrapperProps}
+                isLast={lastItemIndex === index}
+              >
+                <TimeLineCard content={cardContent} isViewDate={false} />
+              </NewsItemWrapper>
+            );
+          })}
+        </ol>
+      </div>
+    </>
   );
 };
 
@@ -246,5 +98,14 @@ const getPropsObjects = (content: NewsItem) => {
   };
   return { wrapperProps, cardContent };
 };
+
+const emptySectionStyle = css({
+  px: '20px',
+  h: '90dvh',
+});
+
+const sectionStyle = css({
+  px: '20px',
+});
 
 const listStyles = flex({ direction: 'column' });
