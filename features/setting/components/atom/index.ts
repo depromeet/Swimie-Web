ea4b5eb42f090d0dfe9c4d53dc45a3e7765ce7c4
summermong <2514o_o@naver.com>
2024-08-26 16:05:35 +0900

    chore: barrel 구조

--- features/setting/components/atom/index.ts
+++ features/setting/components/atom/index.ts
@@ -1,1 +1,3 @@
 export * from './list-item';
+export * from './setting-distance-icon';
+export * from './setting-empty-icon';
--- features/setting/components/organisms/index.ts
+++ features/setting/components/organisms/index.ts
@@ -1,3 +1,4 @@
+export * from './setting-calendar';
 export * from './step-1';
 export * from './step-2';
 export * from './step-3';
