8efca75ee15d13af88574f445aed4f881e41cdd3
hjy0951 <hjy0951@gmail.com>
2024-08-27 01:31:07 +0900

    design: 빈 소식 페이지의 소식 버튼 요소를 link로 변경

--- features/news/components/molecules/empty-news.tsx
+++ features/news/components/molecules/empty-news.tsx
@@ -1,4 +1,6 @@
-import { Button, SpeechBubbleIcon } from '@/components/atoms';
+import Link from 'next/link';
+
+import { SpeechBubbleIcon } from '@/components/atoms';
 import { css } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
 
@@ -12,7 +14,10 @@ export const EmptyNews = () => {
           친구를 만들고 서로의 기록에 응원을 남겨보세요
         </p>
       </div>
-      <Button label="친구 찾기" buttonType="primary" variant="outlined" />
+      {/* TODO: 친구 찾기 페이지로 이동 */}
+      <Link href="/" className={linkStyles}>
+        친구 찾기
+      </Link>
     </div>
   );
 };
@@ -38,4 +43,13 @@ const subTitleStyles = css({
   textStyle: 'body2.normal',
   fontWeight: 'normal',
   color: 'text.alternative',
+});
+
+const linkStyles = css({
+  p: '9px 20px',
+  borderWidth: '2px',
+  borderRadius: '8px',
+  color: 'primary.swim.총거리.default',
+  textStyle: 'body2.normal',
+  fontWeight: 'semibold',
 });
