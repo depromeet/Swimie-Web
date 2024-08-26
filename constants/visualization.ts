9be0f33ce7b1f08c9d9ea28dfacc880204dfd5fc
hjy0951 <hjy0951@gmail.com>
2024-08-27 03:24:01 +0900

    fix: 잘못된 영법별 시각화 색상 변경

--- constants/visualization.ts
+++ constants/visualization.ts
@@ -3,8 +3,8 @@ import { Strokes } from '@/features/main';
 export const swims: Array<{ name: keyof Strokes; color: string }> = [
   { name: '총거리', color: '#3B87F4' },
   { name: '자유형', color: '#3B87F4' },
-  { name: '평영', color: '#F3DD6E' },
-  { name: '배영', color: '#EB5A3F' },
-  { name: '접영', color: '#88D4B0' },
+  { name: '배영', color: '#F3DD6E' },
+  { name: '평영', color: '#88D4B0' },
+  { name: '접영', color: '#EB5A3F' },
   { name: '킥판', color: '#C383EA' },
 ];
