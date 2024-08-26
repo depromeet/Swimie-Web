923bc1b3282989932bc8d9daf607bedee7b107f4
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-26 17:35:35 +0900

    chore: use toast 주석 추가

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -13,6 +13,14 @@ const defaultOptions = {
   delay: 2000,
 } as const;
 
+/**
+ * @description Toast를 조작하기 위해 사용합니다.
+ *
+ * 사용 방법은 아래와 같습니다.
+ * const { toast } = useToast();
+ *
+ * toast('토스트 메세지', { type: 'success', delay: '300' })
+ */
 export const useToast = () => {
   const setToastState = useSetAtom(toastAtom);
 
