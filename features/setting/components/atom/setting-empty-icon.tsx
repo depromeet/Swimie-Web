08ca5dcf15bead33baaa0a91d61100af18825269
summermong <2514o_o@naver.com>
2024-08-26 15:58:16 +0900

    design: 목표 거리 수정에서 사용되는 아이콘 컴포넌트

---
+++ features/setting/components/atom/setting-empty-icon.tsx
@@ -0,0 +1,31 @@
+export default function SettingEmptyIcon() {
+  return (
+    <svg
+      width="45"
+      height="60"
+      viewBox="0 0 45 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Component 12">
+        <rect
+          id="Rectangle 2098"
+          x="0.0714722"
+          width="44"
+          height="60"
+          rx="2"
+          fill="#F7F7F8"
+        />
+        <rect
+          id="Rectangle 2099"
+          x="2.07147"
+          y="2"
+          width="40"
+          height="56"
+          rx="7"
+          fill="#F7F7F8"
+        />
+      </g>
+    </svg>
+  );
+}
