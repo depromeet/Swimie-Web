cceaf03167d90c04e0da63f9d4d0433f328ad872
Kim Hyeonmin <90752841+wokbjso@users.noreply.github.com>
2024-08-26 23:18:18 +0900

    feat: 프로필 선택 바텀 시트 마크업을 생성

--- features/profile/components/organisms/profile-edit-image-section.tsx
+++ features/profile/components/organisms/profile-edit-image-section.tsx
@@ -1,82 +1,56 @@
 'use client';
 
-import { ChangeEvent, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { DefaultImageIcon, Image } from '@/components/atoms';
-import { UserImageIcon } from '@/components/atoms/icons/user-image-icon';
+import { useBottomSheet } from '@/hooks';
 import { css } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
-import { resizeFile } from '@/utils';
 
-interface ProfileEditImageSectionProps {
-  onChange: (file: File) => void;
-}
+import { ProfileEditImageSectionProps } from '../../type';
+import { DefaultProfile } from '../atoms';
+import { ProfileImageBottomSheet } from './profile-image-bottom-sheet';
 
 export function ProfileEditImageSection({
-  onChange,
+  defaultProfileIndex,
+  onChangeFile,
+  onChangeDefaultProfileIndex,
 }: ProfileEditImageSectionProps) {
   const [image, setImage] = useState<string>();
-  const fileInput = useRef<HTMLInputElement>(null);
+  const { isOpen, open, close } = useBottomSheet();
 
-  const handleProfileImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    const uploadImage = async () => {
-      try {
-        if (e.target.files) {
-          const targetFile = e.target.files[0];
-          if (targetFile) setImage('');
-          const resizedImage = await resizeFile(targetFile, 200, 200, 100);
-          const reader = new FileReader();
-          reader.onload = () => {
-            if (reader.readyState === FileReader.DONE) {
-              if (reader.result !== image) {
-                setImage(reader.result as string);
-                onChange(resizedImage);
-              }
-            }
-          };
-          reader.readAsDataURL(resizedImage);
-        }
-      } catch (error) {
-        console.error('이미지 업로드 중 오류가 발생하였습니다', error);
-      }
-    };
-    uploadImage().catch((error) =>
-      console.error('이미지 업로드 중 오류가 발생하였습니다', error),
-    );
-  };
-
-  const handleAddImageClick = () => {
-    if (fileInput.current) {
-      fileInput.current.click();
-    }
+  const handleChangeImage = (image?: string) => {
+    setImage(image);
   };
 
   return (
-    <section className={layoutStyles.imageEdit}>
-      <div className={layoutStyles.imageEditIcon}>
-        {image ? (
-          <Image
-            src={image}
-            alt="프로필 사진"
-            fill
-            sizes="40vw"
-            className={css({ borderRadius: 'full' })}
-          />
-        ) : (
-          <UserImageIcon width={100} height={100} />
-        )}
-        <div className={layoutStyles.defaultImageIcon}>
-          <DefaultImageIcon onClick={handleAddImageClick} />
+    <>
+      <section className={layoutStyles.imageEdit}>
+        <div className={layoutStyles.imageEditIcon}>
+          {image ? (
+            <Image
+              src={image}
+              alt="프로필 사진"
+              fill
+              sizes="40vw"
+              className={css({ borderRadius: 'full' })}
+            />
+          ) : (
+            <DefaultProfile size="big" profileIndex={defaultProfileIndex} />
+          )}
+          <div className={layoutStyles.defaultImageIcon}>
+            <DefaultImageIcon onClick={open} />
+          </div>
         </div>
-      </div>
-      <input
-        ref={fileInput}
-        type="file"
-        accept="image/*"
-        className={css({ display: 'none' })}
-        onChange={handleProfileImageUpload}
+      </section>
+      <ProfileImageBottomSheet
+        isOpen={isOpen}
+        onClose={close}
+        onChangeFile={onChangeFile}
+        onChangeImage={handleChangeImage}
+        onChangeDefaultProfileIndex={onChangeDefaultProfileIndex}
       />
-    </section>
+    </>
   );
 }
 
