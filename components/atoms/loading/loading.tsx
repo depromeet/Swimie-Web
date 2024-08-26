e5aebd0bdec335dea912885c4eb9ef1dd1f70857
hjy0951 <hjy0951@gmail.com>
2024-08-26 21:15:55 +0900

    design: loading area 컴포넌트y축 패딩 세분화

--- components/atoms/loading/loading.tsx
+++ components/atoms/loading/loading.tsx
@@ -1,15 +1,20 @@
-import { css } from '@/styled-system/css';
+import { css, cva } from '@/styled-system/css';
 
 import { LoadingIcon } from '../icons';
 
 interface LoadingScale {
   width?: number;
   height?: number;
+  paddingY?: 'none' | 'small' | 'normal';
 }
 
-export const LoadingArea = ({ width = 20, height = 20 }: LoadingScale) => {
+export const LoadingArea = ({
+  width = 20,
+  height = 20,
+  paddingY = 'normal',
+}: LoadingScale) => {
   return (
-    <div className={containerStyle}>
+    <div className={containerStyle({ paddingY })}>
       <div className={wrapperStyle}>
         <LoadingIcon width={width} height={height} />
       </div>
@@ -17,9 +22,17 @@ export const LoadingArea = ({ width = 20, height = 20 }: LoadingScale) => {
   );
 };
 
-const containerStyle = css({
-  width: 'full',
-  py: '40px',
+const containerStyle = cva({
+  base: {
+    width: 'full',
+  },
+  variants: {
+    paddingY: {
+      none: { py: '0' },
+      small: { py: '20px' },
+      normal: { py: '40px' },
+    },
+  },
 });
 
 const wrapperStyle = css({
