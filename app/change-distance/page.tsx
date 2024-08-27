2a664eab7a63656f819a641a85ba17c0f8d8c2cd
hjy0951 <hjy0951@gmail.com>
2024-08-27 16:40:01 +0900

    refactor: 공통 훅 통합 및 적용

--- app/change-distance/page.tsx
+++ app/change-distance/page.tsx
@@ -5,13 +5,13 @@ import { useEffect, useState } from 'react';
 
 import { LoadingArea } from '@/components/atoms';
 import { BackButton, HeaderBar } from '@/components/molecules';
-import { useMemberData } from '@/features/setting/apis';
 import { SettingCalendar } from '@/features/setting/components';
 import { useSaveDialogHandler } from '@/features/setting/hooks';
+import { useCurrentMemberInfo } from '@/hooks';
 
 export default function Page() {
   const router = useRouter();
-  const { data, error } = useMemberData();
+  const { data, error } = useCurrentMemberInfo();
   const { openSaveModal } = useSaveDialogHandler();
 
   const [selectedDistance, setSelectedDistance] = useState<number | null>(null);
