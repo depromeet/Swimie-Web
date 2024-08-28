af0fbe223af9b6fa601fb55710fb2d53c02e4d4c
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-29 02:46:52 +0900

    feat: isLoading 사용으로 변경

--- features/follow/sections/following-section.tsx
+++ features/follow/sections/following-section.tsx
@@ -11,7 +11,7 @@ export const FollowingSection = ({ id }: { id: number }) => {
     hasNextPage,
     isFetchingNextPage,
     fetchNextPage,
-    isFetching,
+    isLoading,
   } = useFollowingList(id);
 
   const fetchNextData = () => {
@@ -20,7 +20,7 @@ export const FollowingSection = ({ id }: { id: number }) => {
     }
   };
 
-  if (!flattenData.length && !isFetching) {
+  if (!flattenData.length && !isLoading) {
     return <EmptyFollowList type="following" />;
   }
   return <ProfileList data={flattenData} fetchNextData={fetchNextData} />;
