cf61695356f76483cff8ace50a0489a89e4e915a
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-30 01:01:59 +0900

    feat: follow state sync hook, store, api 추가

---
+++ hooks/apis/use-follow-mutate.ts
@@ -0,0 +1,29 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+
+export type RemoveCheer = {
+  reactionId: number;
+};
+
+const fetchFollow = async (followingId: number) => {
+  const res = await fetch(`/api/friend`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ followingId }),
+  });
+
+  return res.json();
+};
+
+export const useFollowMutate = () => {
+  const mutate = useMutation({
+    mutationFn: (followingId: number) => fetchFollow(followingId),
+  });
+
+  return {
+    ...mutate,
+  };
+};
--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,7 @@
 export * from './bottom-sheet';
 export * from './calendar';
 export * from './dialog';
+export * from './member-following';
 export * from './modal';
 export * from './toast';
 export * from './withdrawal-reason';
