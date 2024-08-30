59cddaed684afc666cc0baf1aca062ce889eb2d3
summermong <2514o_o@naver.com>
2024-08-30 15:39:57 +0900

    chore: 애플 로그인 관련 type

---
+++ global.d.ts
@@ -0,0 +1,41 @@
+export {};
+
+declare global {
+  interface Window {
+    AppleID: {
+      auth: {
+        init: (config: ClientConfig) => void;
+        signIn: (config?: ClientConfig) => Promise<SigninResponse>;
+      };
+    };
+  }
+}
+
+interface ClientConfig {
+  clientId: string;
+  redirectURI: string;
+  scope?: string;
+  state?: string;
+  nonce?: string;
+  usePopup?: boolean;
+}
+
+interface Authorization {
+  code: string;
+  id_token: string;
+  state?: string;
+}
+
+interface User {
+  email: string;
+  name: string;
+}
+
+interface SigninResponse {
+  authorization: Authorization;
+  user?: User;
+}
+
+interface SigninError {
+  error: string;
+}
