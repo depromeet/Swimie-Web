804c050f9610d3016833bd98f6df430951ca5a59
hjy0951 <hjy0951@gmail.com>
2024-08-31 01:45:00 +0900

    design: 첫 달력 렌더링시 스켈레톤 추가

--- features/main/components/calendar/atoms/calendar-item-skeleton.tsx
+++ features/main/components/calendar/atoms/calendar-item-skeleton.tsx
@@ -1,8 +1,22 @@
 import { css, cx } from '@/styled-system/css';
+import { flex } from '@/styled-system/patterns';
 
 import { linkContainerStyles, totalDistanceAreaStyles } from './calendar-item';
 
 export const CalendarItemSkeleton = () => {
+  return (
+    <div className={skeletonItemStyles}>
+      <CalendarItemSkeletonLayout />
+      <CalendarItemSkeletonBody />
+    </div>
+  );
+};
+
+export const CalendarItemSkeletonLayout = () => {
+  return <div className={skeletonLayoutStyles} />;
+};
+
+export const CalendarItemSkeletonBody = () => {
   return (
     <>
       <div className={cx(linkContainerStyles, animationStyle)} />
@@ -12,5 +26,15 @@ export const CalendarItemSkeleton = () => {
 };
 
 const animationStyle = css({
+  animation: 'skeleton 1.5s infinite',
+});
+
+const skeletonItemStyles = flex({ direction: 'column', gap: '2px' });
+
+const skeletonLayoutStyles = css({
+  pb: '2px',
+  width: 'full',
+  height: '18px',
+  backgroundColor: 'fill.normal',
   animation: 'skeleton 1.5s infinite',
 });
