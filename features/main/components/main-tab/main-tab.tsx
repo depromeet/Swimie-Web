8c6cdee9f7ec6884752a31ecd6c05d39e1d9318e
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-08-31 00:51:29 +0900

    feat: time line skeleton 적용

--- features/main/components/main-tab/main-tab.tsx
+++ features/main/components/main-tab/main-tab.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import { Tab, TabItem } from '@/components/molecules';
 
 export interface TabItemInfo {
@@ -6,11 +10,35 @@ export interface TabItemInfo {
   onClick: VoidFunction;
 }
 
+export type MainTabType = 'calendar' | 'time-line';
+
 interface MainTabProps {
-  tabInfos: Array<TabItemInfo>;
+  tab: MainTabType;
 }
 
-export const MainTab = ({ tabInfos }: MainTabProps) => {
+export const MainTab = ({ tab }: MainTabProps) => {
+  const router = useRouter();
+
+  const tabInfos: Array<TabItemInfo> = [
+    {
+      text: '캘린더',
+      selected: tab === 'calendar',
+      onClick: () => handleClickTab('calendar'),
+    },
+    {
+      text: '타임라인',
+      selected: tab === 'time-line',
+      onClick: () => handleClickTab('time-line'),
+    },
+  ];
+
+  const handleClickTab = (tab: MainTabType) => {
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set('tab', tab);
+
+    router.replace(currentUrl.toString());
+  };
+
   return (
     <Tab>
       {tabInfos.map((info, index) => (
--- features/main/components/time-line/organisms/time-line.tsx
+++ features/main/components/time-line/organisms/time-line.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Fragment } from 'react';
 
 import { SwimmerIcon } from '@/components/atoms';
