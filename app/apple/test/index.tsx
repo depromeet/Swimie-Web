ad9a96d43e01f04a9e1cdc2da388efdcaf3334fa
summermong <2514o_o@naver.com>
2024-08-31 14:00:17 +0900

    feat: 애플 로그인 구현 테스트

---
+++ app/apple/test/index.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+'use client';
+
+import { useSetAtom } from 'jotai';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { LoginLoading, LoginScreen } from '@/features/login';
+import { AuthInfoAtom } from '@/store/auth';
+import { LoginResponse } from '@/types/authType';
+
+const Page = () => {
+  const router = useRouter();
+  const setAuth = useSetAtom(AuthInfoAtom);
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const data = url.searchParams.get('data') as string;
+    const jsonData = JSON.parse(data) as LoginResponse;
+
+    setAuth({
+      isLogined: true,
+      nickname: jsonData.data.nickname,
+      userId: jsonData.data.userId,
+    });
+
+    if (jsonData.data.isSignUpComplete) {
+      router.push('/');
+    } else {
+      router.push('/join/nickname');
+    }
+  }, [router]);
+
+  return (
+    <>
+      <LoginLoading />
+      <LoginScreen isAnimate={false} />
+    </>
+  );
+};
+
+export default Page;
