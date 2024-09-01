40ad3e25f569a1a440256845f2276fcca1e920eb
summermong <2514o_o@naver.com>
2024-09-02 02:25:40 +0900

    chore: 애플 로그인 구현 테스트

--- app/api/apple/oauth/route.ts
+++ app/api/apple/oauth/route.ts
@@ -6,55 +6,48 @@ import { setAuthCookies } from '@/apis/server-cookie';
 import { LoginResponse } from '@/types/authType';
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
-  try {
-    const body = await request.text();
-    const formData = parse(body);
+  const body = await request.text();
+  const formData = parse(body);
 
-    const code = formData['code'];
-    const idToken = formData['id_token'];
-    const userData = formData['user'];
+  const code = formData['code'];
+  const idToken = formData['id_token'];
+  const userData = formData['user'];
 
-    if (!code || !idToken) {
-      return NextResponse.json(
-        { error: 'code 또는 id_token이 누락되었습니다.' },
-        { status: 400 },
-      );
-    }
-
-    const bodyData = {
-      code: code.toString(),
-      idToken: idToken.toString(),
-      user: userData ? JSON.parse(userData.toString()) : undefined,
-    };
-
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/login/apple`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(bodyData),
-      },
+  if (!code || !idToken) {
+    return NextResponse.json(
+      { error: 'code 또는 id_token이 누락되었습니다.' },
+      { status: 400 },
     );
-
-    if (!res.ok) {
-      return NextResponse.json(
-        { error: '서버 요청 실패' },
-        { status: res.status },
-      );
-    }
-
-    const data = (await res.json()) as LoginResponse;
-
-    setAuthCookies(data.data);
-
-    const loginUrl = new URL('/apple/test', request.url);
-    loginUrl.searchParams.set('data', JSON.stringify(data));
-
-    return NextResponse.redirect(loginUrl);
-  } catch (error) {
-    console.error('Error handling POST request:', error);
-    return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
   }
+
+  const bodyData = {
+    code: code.toString(),
+    idToken: idToken.toString(),
+    user: userData ? JSON.parse(userData.toString()) : undefined,
+  };
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/login/apple`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bodyData),
+  });
+
+  if (!res.ok) {
+    return NextResponse.json(
+      { error: '서버 요청 실패' },
+      { status: res.status },
+    );
+  }
+
+  const data = (await res.json()) as LoginResponse;
+
+  setAuthCookies(data.data);
+
+  // const loginUrl = new URL('/apple/test', request.url);
+  // loginUrl.searchParams.set('data', JSON.stringify(data));
+  const loginUrl = new URL('/');
+
+  return NextResponse.redirect(loginUrl);
 }
