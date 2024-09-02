4eca6314def797235f98a79126ad4617dc2b0e96
brian <90752841+wokbjso@users.noreply.github.com>
2024-09-03 03:14:09 +0900

    feat: time-picker 교체

--- features/record/components/organisms/time-bottom-sheet.tsx
+++ features/record/components/organisms/time-bottom-sheet.tsx
@@ -1,33 +1,29 @@
 'use client';
 
-import { ConfigProvider, TimePicker } from 'antd';
 import dayjs from 'dayjs';
 import { useAtom } from 'jotai';
+import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
+import Picker from 'react-mobile-picker';
 
 import { Button } from '@/components/atoms';
 import { BottomSheet } from '@/components/molecules';
 import { usePreventBodyScroll } from '@/hooks';
 import { css } from '@/styled-system/css';
-import { flex } from '@/styled-system/patterns';
 
-import { useTimeBottomSheet } from '../../hooks';
 import { timeBottomSheetState } from '../../store';
 
 export function TimeBottomSheet() {
   const { setValue } = useFormContext();
   const [timeBottmSheetState, setTimeBottmSheetState] =
     useAtom(timeBottomSheetState);
-  const { width, showTimePicker } = useTimeBottomSheet(
-    timeBottmSheetState.isOpen,
-  );
 
   usePreventBodyScroll({ isOpen: timeBottmSheetState.isOpen });
 
   const handleTimeChange = (date: dayjs.Dayjs) => {
     setTimeBottmSheetState((prev) => ({ ...prev, time: date.format('HH:mm') }));
   };
-
+  console.log(handleTimeChange);
   const handleDoneButtonClick = () => {
     if (timeBottmSheetState.variant === 'start')
       setValue('startTime', timeBottmSheetState.time);
@@ -35,6 +31,44 @@ export function TimeBottomSheet() {
       setValue('endTime', timeBottmSheetState.time);
     setTimeBottmSheetState((prev) => ({ ...prev, isOpen: false }));
   };
+
+  const selections = {
+    ampm: ['오전', '오후'],
+    hour: [
+      '01',
+      '02',
+      '03',
+      '04',
+      '05',
+      '06',
+      '07',
+      '08',
+      '09',
+      '10',
+      '11',
+      '12',
+    ],
+    minute: [
+      '00',
+      '05',
+      '10',
+      '15',
+      '20',
+      '25',
+      '30',
+      '35',
+      '40',
+      '45',
+      '50',
+      '55',
+    ],
+  };
+
+  const [pickerValue, setPickerValue] = useState({
+    ampm: '오후',
+    hour: '02',
+    minute: '05',
+  });
 
   return (
     <BottomSheet
@@ -48,51 +82,35 @@ export function TimeBottomSheet() {
       }}
       isRenderHandlebar
     >
-      <ConfigProvider
-        theme={{
-          components: {
-            DatePicker: {
-              timeCellHeight: 36,
-              timeColumnWidth: Math.floor(width / 3),
-              timeColumnHeight: 116,
-            },
-          },
-          token: {
-            motion: false,
-          },
-        }}
-      >
-        <div className={layoutStyles.bottomSheet}>
-          <TimePicker
-            placeholder="시간 설정"
-            format="HH:mm"
-            use12Hours
-            suffixIcon={null}
-            popupClassName={css({
-              opacity: showTimePicker ? 1 : 0,
-              transition: 'opacity 0.3s ease-in-out',
-            })}
-            panelRender={(originPanel) =>
-              showTimePicker ? (
-                <div className={panelStyles}>{originPanel}</div>
-              ) : null
-            }
-            open={timeBottmSheetState.isOpen}
-            showNow={false}
-            inputMode="none"
-            size="large"
-            placement="bottomRight"
-            needConfirm={false}
-            inputReadOnly
-            onPickerValueChange={handleTimeChange}
-            variant="borderless"
-            className={css({
-              width: '100%',
-              '& input': { visibility: 'hidden' },
-            })}
-          />
-        </div>
-      </ConfigProvider>
+      <div className={css({ padding: '0 20px' })}>
+        <Picker
+          value={pickerValue}
+          onChange={setPickerValue}
+          wheelMode="natural"
+          height={120}
+          itemHeight={34}
+          className={layoutStyles.picker}
+        >
+          {Object.keys(selections).map((name) => (
+            <Picker.Column key={name} name={name}>
+              {selections[name as keyof typeof selections].map((option) => (
+                <Picker.Item key={option} value={option}>
+                  {({ selected }) => (
+                    <span
+                      className={css({
+                        color: selected ? 'text.normal' : 'text.alternative',
+                      })}
+                    >
+                      {option}
+                    </span>
+                  )}
+                </Picker.Item>
+              ))}
+            </Picker.Column>
+          ))}
+        </Picker>
+      </div>
+
       <div className={layoutStyles.button}>
         <Button
           buttonType="primary"
@@ -109,13 +127,6 @@ export function TimeBottomSheet() {
 }
 
 const layoutStyles = {
-  bottomSheet: flex({
-    direction: 'column',
-    alignItems: 'center',
-    width: '100%',
-    height: '128px',
-    marginBottom: '32px',
-  }),
   button: css({
     w: 'full',
     position: 'absolute',
@@ -123,11 +134,10 @@ const layoutStyles = {
     left: 0,
     padding: '0 20px',
   }),
+  picker: css({
+    marginBottom: '50px',
+    marginTop: '10px',
+    textStyle: 'heading3',
+    fontWeight: 500,
+  }),
 };
-
-const panelStyles = css({
-  position: 'fixed',
-  bottom: '82px',
-  textStyle: 'heading3',
-  fontWeight: 400,
-});
