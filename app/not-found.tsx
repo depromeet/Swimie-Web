91547682508cbd0eca3066a89a9b3b308eb5ba3b
hjy0951 <hjy0951@gmail.com>
2024-09-04 00:25:03 +0900

    design: gnb 크기 고려하여 컨테이너의 높이 재설정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -28,7 +28,7 @@ export default function NotFound() {
 
 const containerStyles = flex({
   width: '100%',
-  height: '90dvh',
+  height: 'calc(100dvh - 66px)',
   direction: 'column',
   gap: '20px',
   justifyContent: 'center',
