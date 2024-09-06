1a050e9818e6ed65e775be091276c3ad4688eb25
brian <90752841+wokbjso@users.noreply.github.com>
2024-09-07 00:48:42 +0900

    feat: 기록 페이지에 경고 문구를 생성

---
+++ components/atoms/icons/warn-icon.tsx
@@ -0,0 +1,13 @@
+export function WarnIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+      <path fill="#121214" d="M8.334 5h3.333v10.833H8.334z" />
+      <path
+        fill="#FFB800"
+        fillRule="evenodd"
+        d="M1.75 10a8.25 8.25 0 1 1 16.5 0 8.25 8.25 0 0 1-16.5 0Zm9.083 3.542a.833.833 0 1 1-1.666 0 .833.833 0 0 1 1.666 0ZM9.25 5.625v5.833h1.5V5.625h-1.5Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
