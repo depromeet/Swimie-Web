49196f6a29a067c8f2beec9bede708719602be60
brian <90752841+wokbjso@users.noreply.github.com>
2024-09-06 18:50:32 +0900

    chore: 변수 이름 수정

--- features/record/server-actions/save-swim-time.ts
+++ features/record/server-actions/save-swim-time.ts
@@ -3,16 +3,19 @@
 import { cookies } from 'next/headers';
 
 export const saveSwimTime = (startTime: string, endTime: string) => {
-  const prevStartTime = cookies().get('startTime')?.value;
-  const prevEndTime = cookies().get('endTime')?.value;
-  if (!prevStartTime || (prevStartTime && prevStartTime !== startTime))
-    cookies().set('startTime', startTime, {
+  const prevSwimStartTime = cookies().get('swimStartTime')?.value;
+  const prevSwimEndTime = cookies().get('swimEndTime')?.value;
+  if (
+    !prevSwimStartTime ||
+    (prevSwimStartTime && prevSwimStartTime !== startTime)
+  )
+    cookies().set('swimStartTime', startTime, {
       maxAge: Infinity,
       httpOnly: true,
       secure: true,
     });
-  if (!prevEndTime || (prevEndTime && prevEndTime !== endTime))
-    cookies().set('endTime', endTime, {
+  if (!prevSwimEndTime || (prevSwimEndTime && prevSwimEndTime !== endTime))
+    cookies().set('swimEndTime', endTime, {
       maxAge: Infinity,
       httpOnly: true,
       secure: true,
