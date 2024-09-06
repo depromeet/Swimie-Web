3fdeaf0d350715831cf78aa3090fcb1c8ba6487f
Jiyoung Jung <72294509+Jungjjeong@users.noreply.github.com>
2024-09-07 00:23:44 +0900

    feat: report section, component 개발

--- features/record-detail/components/index.ts
+++ features/record-detail/components/index.ts
@@ -1,6 +1,7 @@
 export * from './cheer-modal';
 export * from './date-picker';
 export * from './edit-button';
+export * from './report-bottom-sheet';
 export * from './swim-description-item';
 export * from './swim-stats-item';
 export * from './swim-tool-item';
