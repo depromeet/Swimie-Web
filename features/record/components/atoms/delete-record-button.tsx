84c770688c300d85594b4203f498f19eda57fb35
brian <90752841+wokbjso@users.noreply.github.com>
2024-09-06 23:23:11 +0900

    feat: 삭제 버튼 클릭 시 dialog 연결

--- features/record/components/atoms/delete-record-button.tsx
+++ features/record/components/atoms/delete-record-button.tsx
@@ -1,7 +1,34 @@
+import { useDialog } from '@/hooks';
 import { css } from '@/styled-system/css';
 
 export function DeleteRecordButton() {
-  return <button className={buttonStyles}>삭제</button>;
+  const { dialog, close } = useDialog();
+  const handleDeleteButtonClick = () => {
+    dialog({
+      title: '기록을 삭제할까요?',
+      description: `삭제한 기록은 다시 복구할 수 없어요.`,
+      buttons: {
+        cancel: {
+          text: '닫기',
+          onClick: () => {
+            close();
+          },
+        },
+        confirm: {
+          text: '삭제하기',
+          onClick: () => {
+            close();
+          },
+        },
+      },
+      isDim: true,
+    });
+  };
+  return (
+    <button className={buttonStyles} onClick={handleDeleteButtonClick}>
+      삭제
+    </button>
+  );
 }
 
 const buttonStyles = css({
