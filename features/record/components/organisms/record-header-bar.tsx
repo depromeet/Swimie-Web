09b100448f3d22339e2c4921ab49e3b188b69734
brian <90752841+wokbjso@users.noreply.github.com>
2024-09-10 15:53:57 +0900

    chore: 그냥 뒤로가기를 누르는 상황 고려

--- features/record/components/organisms/record-header-bar.tsx
+++ features/record/components/organisms/record-header-bar.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
+import { useSetAtom } from 'jotai';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import { BackButton, HeaderBar } from '@/components/molecules';
 import { css } from '@/styled-system/css';
 
+import { formSubInfoState } from '../../store/form-sub-info';
 import { DeleteRecordButton } from '../atoms';
 
 interface RecordHeaderBarProps {
@@ -12,14 +14,26 @@ interface RecordHeaderBarProps {
 }
 
 export function RecordHeaderBar({ title }: RecordHeaderBarProps) {
+  const router = useRouter();
+
   const searchParams = useSearchParams();
   const memoryId = searchParams.get('memoryId');
   const isEditMode = Boolean(memoryId);
+  const setFormSubInfo = useSetAtom(formSubInfoState);
+
+  const handleBackButtonClick = () => {
+    router.back();
+    setFormSubInfo({
+      imageFiles: [],
+      isDistanceLapModified: false,
+      isPrevImageFileDeleted: false,
+    });
+  };
 
   return (
     <HeaderBar className={css({ marginBottom: '16px' })}>
       <HeaderBar.LeftContent>
-        <BackButton />
+        <BackButton onClickBack={handleBackButtonClick} />
       </HeaderBar.LeftContent>
       <HeaderBar.Title>{title}</HeaderBar.Title>
       {isEditMode && (
