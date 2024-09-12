d91ce60511243d36032e07eff02ed51b0a9b68b4
brian <90752841+wokbjso@users.noreply.github.com>
2024-09-13 00:51:53 +0900

    fix: 미래 날짜 막는 버그 수정

--- features/record/utils/is-future.ts
+++ features/record/utils/is-future.ts
@@ -1,7 +1,11 @@
 export const isFuture = (dateString: string) => {
   const givenDate = new Date(dateString);
 
+  givenDate.setHours(0, 0, 0, 0);
+
   const currentDate = new Date();
+
+  currentDate.setHours(0, 0, 0, 0);
 
   return givenDate > currentDate;
 };
