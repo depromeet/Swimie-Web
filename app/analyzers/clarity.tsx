d69ef7390d8c535cfddb002b528137176432de35
hjy0951 <hjy0951@gmail.com>
2024-09-13 21:17:05 +0900

    chore: 개발 영역 조건문 삭제

--- app/analyzers/clarity.tsx
+++ app/analyzers/clarity.tsx
@@ -1,8 +1,6 @@
 import Script from 'next/script';
 
 export const Clarity = () => {
-  if (process.env.NODE_ENV !== 'production') return null;
-
   return (
     <Script id="clarity" type="text/javascript">
       {`
