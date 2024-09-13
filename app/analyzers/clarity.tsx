60a50090a1cff9e4560f00e8848ddf251d92599b
hjy0951 <hjy0951@gmail.com>
2024-09-13 21:08:08 +0900

    chore: 분석 툴인 clarity 추가

---
+++ app/analyzers/clarity.tsx
@@ -0,0 +1,17 @@
+import Script from 'next/script';
+
+export const Clarity = () => {
+  if (process.env.NODE_ENV !== 'production') return null;
+
+  return (
+    <Script id="clarity" type="text/javascript">
+      {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "o2k3nk4cvl");
+        `}
+    </Script>
+  );
+};
