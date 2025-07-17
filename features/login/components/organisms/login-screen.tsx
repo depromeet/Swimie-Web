e0dbfba40c53229a823d7e1ab6a6f8ae52982774
summermong <2514o_o@naver.com>
2025-07-18 00:37:43 +0900

    feat: 서비스 종료

--- features/login/components/organisms/login-screen.tsx
+++ features/login/components/organisms/login-screen.tsx
@@ -2,17 +2,18 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+//import Link from 'next/link';
+import { useEffect } from 'react';
 
-import {
-  AppleLogoIcon,
-  GoogleLogoIcon,
-  KakaoLogoIcon,
-} from '@/components/atoms/icons';
-import LoginMainCharacter from '@/public/images/login/login-main-character.png';
-import SwimieLetterLogo from '@/public/images/login/swimie-letter-logo.png';
-import { css, cva } from '@/styled-system/css';
+// import {
+//   AppleLogoIcon,
+//   GoogleLogoIcon,
+//   KakaoLogoIcon,
+// } from '@/components/atoms/icons';
+import CloseImg from '@/public/images/close.webp';
+//import LoginMainCharacter from '@/public/images/login/login-main-character.png';
+//import SwimieLetterLogo from '@/public/images/login/swimie-letter-logo.png';
+//import { css, cva } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
 
 import { useLoginFailDialogHandler } from '../../hook/use-login-fail-dialog-handler';
@@ -21,25 +22,25 @@ type LoginScreen = {
   isAnimate?: boolean;
 };
 export const LoginScreen = ({ isAnimate = true }: LoginScreen) => {
-  const [isAppleLoginDisabled, setIsAppleLoginDisabled] = useState(false);
+  //const [isAppleLoginDisabled, setIsAppleLoginDisabled] = useState(false);
   const { openFailModal } = useLoginFailDialogHandler();
 
-  const kakaoLogin = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code&prompt=select_account`;
-  };
+  // const kakaoLogin = () => {
+  //   window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code&prompt=select_account`;
+  // };
 
-  const googleLogin = () => {
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email profile&prompt=consent&access_type=offline`;
-  };
+  // const googleLogin = () => {
+  //   window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email profile&prompt=consent&access_type=offline`;
+  // };
 
-  const appleLogin = () => {
-    setIsAppleLoginDisabled(true);
-    window.location.href = `https://appleid.apple.com/auth/authorize?client_id=${process.env.NEXT_PUBLIC_APPLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APPLE_REDIRECT_URI}&response_type=code id_token&scope=name email&response_mode=form_post`;
+  // const appleLogin = () => {
+  //   setIsAppleLoginDisabled(true);
+  //   window.location.href = `https://appleid.apple.com/auth/authorize?client_id=${process.env.NEXT_PUBLIC_APPLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_APPLE_REDIRECT_URI}&response_type=code id_token&scope=name email&response_mode=form_post`;
 
-    setTimeout(() => {
-      setIsAppleLoginDisabled(false);
-    }, 5000);
-  };
+  //   setTimeout(() => {
+  //     setIsAppleLoginDisabled(false);
+  //   }, 5000);
+  // };
 
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
@@ -64,7 +65,8 @@ export const LoginScreen = ({ isAnimate = true }: LoginScreen) => {
           },
         })}
       >
-        <div className={logoStyles}>
+        <Image src={CloseImg} alt="close" width={0} height={0} />
+        {/* <div className={logoStyles}>
           <div className={logoContainer}>
             <Image
               width={149}
@@ -116,7 +118,7 @@ export const LoginScreen = ({ isAnimate = true }: LoginScreen) => {
             <Link href="/setting/privacy-policy">개인정보 처리방침</Link>에
             동의합니다.
           </p>
-        </div>
+        </div> */}
         {/* TODO: Button 리팩토링 예정 */}
       </motion.div>
     </div>
@@ -133,86 +135,86 @@ const loginPage = flex({
   overflow: 'hidden',
 });
 
-const logoStyles = flex({
-  justifyContent: 'center',
-  alignItems: 'center',
-  direction: 'column',
-  gap: '12px',
-});
+// const logoStyles = flex({
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   direction: 'column',
+//   gap: '12px',
+// });
 
-const logoContainer = flex({
-  direction: 'column',
-  alignItems: 'center',
-  gap: '12px',
-  alignSelf: 'stretch',
-});
+// const logoContainer = flex({
+//   direction: 'column',
+//   alignItems: 'center',
+//   gap: '12px',
+//   alignSelf: 'stretch',
+// });
 
-const characterContainer = flex({
-  position: 'relative',
-  width: 'max-content',
-  height: 'auto',
-  direction: 'column',
-  alignItems: 'center',
-  overflow: 'hidden',
-});
+// const characterContainer = flex({
+//   position: 'relative',
+//   width: 'max-content',
+//   height: 'auto',
+//   direction: 'column',
+//   alignItems: 'center',
+//   overflow: 'hidden',
+// });
 
-const titleStyles = css({
-  color: 'background.white',
-  textStyle: 'heading6',
-  fontWeight: '500',
-  textAlign: 'center',
-});
+// const titleStyles = css({
+//   color: 'background.white',
+//   textStyle: 'heading6',
+//   fontWeight: '500',
+//   textAlign: 'center',
+// });
 
-const loginButtons = flex({
-  padding: '0px 20px',
-  direction: 'column',
-  alignItems: 'flex-start',
-  gap: '8px',
-  alignSelf: 'stretch',
-  margin: '0 auto',
-});
+// const loginButtons = flex({
+//   padding: '0px 20px',
+//   direction: 'column',
+//   alignItems: 'flex-start',
+//   gap: '8px',
+//   alignSelf: 'stretch',
+//   margin: '0 auto',
+// });
 
-const buttonContent = flex({
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: '6px',
-});
+// const buttonContent = flex({
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   gap: '6px',
+// });
 
-const buttons = cva({
-  base: {
-    width: '335px',
-    height: '48px',
-    flexShrink: 0,
-    borderRadius: '10px',
-    cursor: 'pointer',
-    '& span': {
-      textStyle: 'body1.normal',
-      fontWeight: 'bold',
-    },
-  },
-  variants: {
-    type: {
-      kakao: {
-        backgroundColor: '#FFCC00',
-        '& span': { color: 'text.normal' },
-      },
-      google: {
-        backgroundColor: 'background.white',
-        borderColor: 'line.neutral',
-        border: '1px solid',
-        '& span': { color: 'text.neutral' },
-      },
-      apple: {
-        backgroundColor: 'static.black',
-        '& span': { color: 'static.white' },
-      },
-    },
-  },
-});
+// const buttons = cva({
+//   base: {
+//     width: '335px',
+//     height: '48px',
+//     flexShrink: 0,
+//     borderRadius: '10px',
+//     cursor: 'pointer',
+//     '& span': {
+//       textStyle: 'body1.normal',
+//       fontWeight: 'bold',
+//     },
+//   },
+//   variants: {
+//     type: {
+//       kakao: {
+//         backgroundColor: '#FFCC00',
+//         '& span': { color: 'text.normal' },
+//       },
+//       google: {
+//         backgroundColor: 'background.white',
+//         borderColor: 'line.neutral',
+//         border: '1px solid',
+//         '& span': { color: 'text.neutral' },
+//       },
+//       apple: {
+//         backgroundColor: 'static.black',
+//         '& span': { color: 'static.white' },
+//       },
+//     },
+//   },
+// });
 
-const agreementStyles = css({
-  textStyle: 'caption2',
-  fontWeight: 'regular',
-  color: 'line.solid.normal',
-  '& > a': { textDecoration: 'underline' },
-});
+// const agreementStyles = css({
+//   textStyle: 'caption2',
+//   fontWeight: 'regular',
+//   color: 'line.solid.normal',
+//   '& > a': { textDecoration: 'underline' },
+// });
